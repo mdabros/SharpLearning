6c4e110594d203c9c6a1dfd94de7b674f7248fe1
Mads Dabros <m_dabros@hotmail.com>
2014-05-18 16:41:50 +0200

    SharpLearning.Metrics 0.1.6.0: Added MeanAbsolutErrorRegressionMetric and test

---
+++ Source/SharpLearning.Metrics.Test/Regression/MeanAbsolutErrorRegressionMetricTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Metrics.Regression;
+
+namespace SharpLearning.Metrics.Test.Regression
+{
+    [TestClass]
+    public class MeanAbsolutErrorRegressionMetricTest
+    {
+        [TestMethod]
+        public void MeanAbsolutErrorRegressionMetric_Error()
+        {
+            var targets = new double[] { 1.0, 2.3, 3.1, 4.4, 5.8 };
+            var predictions = new double[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            var sut = new MeanAbsolutErrorRegressionMetric();
+
+            var actual = sut.Error(targets, predictions);
+            Assert.AreEqual(0.32, actual, 0.00001);
+        }
+
+        [TestMethod]
+        public void MeanAbsolutErrorRegressionMetric_Error_Zero_Error()
+        {
+            var targets = new double[] { 0.0, 0.0 };
+            var predictions = new double[] { 0.0, 0.0 };
+            var sut = new MeanAbsolutErrorRegressionMetric();
+
+            var actual = sut.Error(targets, predictions);
+            Assert.AreEqual(0.0, actual);
+        }
+    }
+}
---
+++ Source/SharpLearning.Metrics/Regression/MeanAbsolutErrorRegressionMetric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SharpLearning.Metrics.Regression
+{
+    public sealed class MeanAbsolutErrorRegressionMetric : IRegressionMetric
+    {
+        public double Error(double[] targets, double[] predicted)
+        {
+            var meanSquareError = 0.0;
+            for (int i = 0; i < targets.Length; ++i)
+            {
+                var targetValue = targets[i];
+                var estimate = predicted[i];
+                var error = Math.Abs(targetValue - estimate);
+                meanSquareError += error;
+            }
+            meanSquareError *= (1.0 / targets.Length);
+
+            return meanSquareError;
+        }
+    }
+}
