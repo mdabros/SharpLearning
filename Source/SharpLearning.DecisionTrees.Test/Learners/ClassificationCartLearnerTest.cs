b5268beeceeccc50798885cd749cce686af3d89b
Mads Dabros <m_dabros@hotmail.com>
2014-06-08 11:13:17 +0200

    SharpLearning.DecisionTrees.Test 0.3.1.1: Added Timing tests to CartLeaners

--- Source/SharpLearning.DecisionTrees.Test/Learners/ClassificationCartLearnerTest.cs
+++ Source/SharpLearning.DecisionTrees.Test/Learners/ClassificationCartLearnerTest.cs
@@ -7,6 +7,8 @@ using System.IO;
 using SharpLearning.DecisionTrees.Test.Properties;
 using SharpLearning.DecisionTrees.Learners;
 using SharpLearning.Metrics.Classification;
+using SharpLearning.Containers.Matrices;
+using System.Diagnostics;
 
 namespace SharpLearning.DecisionTrees.Test.Learners
 {
@@ -50,6 +52,45 @@ namespace SharpLearning.DecisionTrees.Test.Learners
 
             Assert.AreEqual(0.0, error, 0.0000001);
         }
+
+        [Ignore]
+        [TestMethod]
+        public void ClassificationCartLearner_Learn_Timing()
+        {
+            var rows = 10000;
+            var cols = 10;
+
+            var random = new Random(42);
+            var observations = new F64Matrix(rows, cols);
+            var targets = new double[rows];
+            
+            for (int i = 0; i < targets.Length; i++)
+            {
+                targets[i] = random.Next(5);
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    observations.SetItemAt(i, j, random.NextDouble());   
+                }
+            }
+
+            var sut = new ClassificationCartLearner(1, 100, 0.001);
+            var timer = new Stopwatch();
+            timer.Start();
+            var model = sut.Learn(observations, targets);
+            timer.Stop();
+
+            var predictions = model.Predict(observations);
+
+            var evaluator = new TotalErrorClassificationMetric<double>();
+            var error = evaluator.Error(targets, predictions);
+
+            Trace.WriteLine("Error: " + error);
+            Trace.WriteLine("Time: " + timer.ElapsedMilliseconds);
+        }
+
 
     }
 }
--- Source/SharpLearning.DecisionTrees.Test/Learners/RegressionCartLearnerTest.cs
+++ Source/SharpLearning.DecisionTrees.Test/Learners/RegressionCartLearnerTest.cs
@@ -34,5 +34,44 @@ namespace SharpLearning.DecisionTrees.Test.Learners
 
             Assert.AreEqual(0.038873687234849852, error, 0.0000001);
         }
+
+        [Ignore]
+        [TestMethod]
+        public void RegressionCartLearner_Learn_Timing()
+        {
+            var rows = 10000;
+            var cols = 10;
+
+            var random = new Random(42);
+            var observations = new F64Matrix(rows, cols);
+            var targets = new double[rows];
+
+            for (int i = 0; i < targets.Length; i++)
+            {
+                targets[i] = random.Next(5);
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    observations.SetItemAt(i, j, random.NextDouble());
+                }
+            }
+
+            var sut = new RegressionCartLearner(1, 100, 0.001);
+            var timer = new Stopwatch();
+            timer.Start();
+            var model = sut.Learn(observations, targets);
+            timer.Stop();
+
+            var predictions = model.Predict(observations);
+
+            var evaluator = new MeanSquaredErrorRegressionMetric();
+            var error = evaluator.Error(targets, predictions);
+
+            Trace.WriteLine("Error: " + error);
+            Trace.WriteLine("Time: " + timer.ElapsedMilliseconds);
+        }
+
     }
 }
