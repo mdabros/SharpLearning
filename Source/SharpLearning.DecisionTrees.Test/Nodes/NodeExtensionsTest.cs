c0d3dfb47f7bb819f4d0b5c740172eb4fd524c26
Mads Dabros <m_dabros@hotmail.com>
2014-07-27 13:55:53 +0200

    SharpLearning.DecisionTrees 0.8.8.1: Added unittests for LeadNodeFactory and NodeExtensions.

---
+++ Source/SharpLearning.DecisionTrees.Test/Nodes/NodeExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.DecisionTrees.Nodes;
+using System.Collections.Generic;
+
+namespace SharpLearning.DecisionTrees.Test.Nodes
+{
+    [TestClass]
+    public class NodeExtensionsTest
+    {
+        [TestMethod]
+        public void NodeExtensions_UpdateParent()
+        {
+            var nodes = new List<INode> { new SplitNode(-1, 2.0, -1, -1, 0) };
+            var left = new LeafNode(1, 5.0, 1);
+            var right = new LeafNode(1, 5.0, 2);
+
+            nodes.UpdateParent(nodes[0], left, NodePositionType.Left);
+            Assert.AreEqual(nodes[0].LeftIndex, left.NodeIndex);
+
+            nodes.UpdateParent(nodes[0], right, NodePositionType.Right);
+            Assert.AreEqual(nodes[0].LeftIndex, left.NodeIndex);
+            Assert.AreEqual(nodes[0].RightIndex, right.NodeIndex);
+        }
+    }
+}
