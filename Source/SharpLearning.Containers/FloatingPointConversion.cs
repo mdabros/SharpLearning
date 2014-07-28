ea90c653d5d774d0f436cfabb773f4093be1042a
Mads Dabros <m_dabros@hotmail.com>
2014-07-28 17:54:01 +0200

    SharpLearning.Containers 0.8.8.3: Added NumberFormatInfo as member in FLoatingPointConversion to reduce memory use.

--- Source/SharpLearning.Containers/FloatingPointConversion.cs
+++ Source/SharpLearning.Containers/FloatingPointConversion.cs
@@ -5,13 +5,13 @@ namespace SharpLearning.Containers
     public static class FloatingPointConversion
     {
         public const string DefaultFormat = "R";
+        public static readonly NumberFormatInfo nfi = new NumberFormatInfo();
 
         /// <summary>
         /// Default format for outputting double values to string.
         /// </summary> 
         public static string ToString(double value)
         {
-            var nfi = new NumberFormatInfo();
             return value.ToString(DefaultFormat, nfi);
         }
 
@@ -22,7 +22,6 @@ namespace SharpLearning.Containers
         /// <returns></returns>
         public static double ToF64(string value)
         {
-            var nfi = new NumberFormatInfo();
             return double.Parse(value, nfi);
         }
     }
