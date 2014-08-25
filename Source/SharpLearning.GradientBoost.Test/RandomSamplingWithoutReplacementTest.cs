2a37b2d60927ef5690949b987335fbc4ce06bfee
Mads Dabros <m_dabros@hotmail.com>
2014-08-25 18:31:05 +0200

    SharpLearning.GradientBoost 0.11.6.0: Added RandomSamplingWithoutReplacement for Stochastic Gradient Boosting.

---
+++ Source/SharpLearning.GradientBoost.Test/RandomSamplingWithoutReplacementTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace SharpLearning.GradientBoost.Test
+{
+    [TestClass]
+    public class RandomSamplingWithoutReplacementTest
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RandomSamplingWithoutReplacement_Percentage_Too_Small()
+        {
+            new RandomSamplingWithoutReplacement(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RandomSamplingWithoutReplacement_Percentage_Too_Big()
+        {
+            new RandomSamplingWithoutReplacement(1.1);
+        }
+
+
+        [TestMethod]
+        public void RandomSamplingWithoutReplacement_Sample_100()
+        {
+            var sut = new RandomSamplingWithoutReplacement(1.0);
+            var indices = Enumerable.Range(0, 10).ToArray();
+
+            var actual = new int[indices.Length];
+            sut.Sample(indices, ref actual);
+
+            CollectionAssert.AreEqual(indices, actual.OrderBy(v => v).ToArray());
+        }
+
+        [TestMethod]
+        public void RandomSamplingWithoutReplacement_Sample_50()
+        {
+            var sut = new RandomSamplingWithoutReplacement(.5);
+            var indices = Enumerable.Range(0, 10).ToArray();
+
+            var actual = new int[indices.Length];
+            sut.Sample(indices, ref actual);
+
+            var expected = new int[] { 9, 0, 4, 2, 5 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Source/SharpLearning.GradientBoost/RandomSamplingWithoutReplacement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpLearning.Containers;
+
+namespace SharpLearning.GradientBoost
+{
+    public sealed class RandomSamplingWithoutReplacement
+    {
+        readonly Random m_random;
+        readonly double m_percentage;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="percentage">Percentage of full size to sample (0.0 to 1.0)</param>
+        public RandomSamplingWithoutReplacement(double percentage)
+            : this(percentage, 42)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="percentage">Percentage of full size to sample (0.0 to 1.0)</param>
+        /// <param name="seed">Random seed</param>
+        public RandomSamplingWithoutReplacement(double percentage, int seed)
+        {
+            if (percentage < 0.0 || percentage > 1.0) { throw new ArgumentException("Percentage must be between 0.0 and 1.0"); }
+            m_percentage = percentage;
+            m_random = new Random(seed);
+        }
+
+        /// <summary>
+        /// Random sampling using Fischer-Yates shuffle:
+        /// http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+        /// </summary>
+        /// <param name="indices"></param>
+        /// <param name="outIndices"></param>
+        public void Sample(int[] indices, ref int[] outIndices)
+        {
+            Array.Copy(indices, outIndices, indices.Length);
+            outIndices.Shuffle(m_random);
+            var newLength = (int)(indices.Length * m_percentage);
+
+            Array.Resize(ref outIndices, newLength);
+        }
+    }
+}
