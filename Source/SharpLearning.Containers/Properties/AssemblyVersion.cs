0e103bd9d3e86db119737e7833a4df40bead6346
Mads Dabros <m_dabros@hotmail.com>
2015-01-04 11:38:41 +0100

    Incremented AssemblyVersion

--- Source/SharpLearning.Containers/Properties/AssemblyVersion.cs
+++ Source/SharpLearning.Containers/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.14.*")]
+[assembly: AssemblyVersion("0.15.*")]
 [assembly: AssemblyFileVersion("0.15.0.0")]
 // SharpLearning
