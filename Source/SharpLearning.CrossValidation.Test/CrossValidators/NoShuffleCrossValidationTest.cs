1a9b7028a46dd4145b1d232f40d29a9727ff3c7b
Mads Dabros <m_dabros@hotmail.com>
2015-01-04 11:54:03 +0100

    SharpLearning.CrossValidation 0.15.2.0: Added NoShuffleCrossValidation and test.

---
+++ Source/SharpLearning.CrossValidation.Test/CrossValidators/NoShuffleCrossValidationTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Containers.Matrices;
+using SharpLearning.CrossValidation.Test;
+using System;
+using System.Linq;
+
+namespace SharpLearning.CrossValidation.CrossValidators.Test
+{
+    [TestClass]
+    public class NoShuffleCrossValidationTest
+    {
+        [TestMethod]
+        public void NoShuffleCrossValidation_CrossValidate_Folds_2()
+        {
+            var actual = AssertCrossValidation(2);
+            var expected = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NoShuffleCrossValidation_CrossValidate_Folds_10()
+        {
+            var actual = AssertCrossValidation(10);
+            var expected = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NoShuffleCrossValidation_CrossValidate_Too_Many_Folds()
+        {
+            AssertCrossValidation(20);
+        }
+
+        double[] AssertCrossValidation(int folds)
+        {
+            var observations = new F64Matrix(10, 10);
+            var targets = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var indices = Enumerable.Range(0, targets.Length).ToArray();
+
+            var sut = new NoShuffleCrossValidation<double>(folds);
+            var actual = sut.CrossValidate(() => new CrossValidationTestLearner(indices), observations, targets);
+            return actual;
+        }
+    }
+}
---
+++ Source/SharpLearning.CrossValidation/CrossValidators/NoShuffleCrossValidation.cs
@@ -0,0 +1,20 @@
+﻿using SharpLearning.CrossValidation.Shufflers;
+
+namespace SharpLearning.CrossValidation.CrossValidators
+{
+    /// <summary>
+    /// No shuffle cross validation does not shuffle the the observation indices and keeps the original order.
+    /// </summary>
+    /// <typeparam name="TPrediction"></typeparam>
+    public sealed class NoShuffleCrossValidation<TPrediction> : CrossValidation<TPrediction>
+    {
+        /// <summary>
+        /// Cross validation for evaluating how learning algorithms generalise on new data
+        /// </summary>
+        /// <param name="crossValidationFolds">Number of folds that should be used for cross validation</param>
+        public NoShuffleCrossValidation(int crossValidationFolds)
+            : base(new NoShuffleCrossValidationShuffler<double>(), crossValidationFolds)
+        {
+        }
+    }
+}
