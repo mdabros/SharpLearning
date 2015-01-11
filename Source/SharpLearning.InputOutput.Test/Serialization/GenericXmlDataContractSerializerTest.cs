94363769fedfb444e75a2b2a230247e7e98d3890
Mads Dabros <m_dabros@hotmail.com>
2015-01-11 15:47:11 +0100

    SharpLearning.InputOutput 0.15.6.0: Added GenericXmlDataContractSerializer

---
+++ Source/SharpLearning.InputOutput.Test/Serialization/GenericXmlDataContractSerializerTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.InputOutput.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharpLearning.InputOutput.Test.Serialization
+{
+    [TestClass]
+    public class GenericXmlDataContractSerializerTest
+    {
+        [TestMethod]
+        public void GenericXmlDataContractSerializer_Serialize()
+        {
+            var writer = new StringWriter();
+            
+            GenericXmlDataContractSerializer.Serialize(SerializationData,
+                () => writer);
+
+            Assert.AreEqual(SerializationString, writer.ToString());
+        }
+
+        [TestMethod]
+        public void GenericXmlDataContractSerializer_Deserialize()
+        {
+            var reader = new StringReader(SerializationString);
+
+            var actual = GenericXmlDataContractSerializer.Deserialize<Dictionary<string, int>>(
+                () => reader);
+
+            CollectionAssert.AreEqual(SerializationData, actual);
+        }
+
+        Dictionary<string, int> SerializationData = new Dictionary<string, int>
+        {
+            {"Test1", 0}, {"Test2", 1}, {"Test3", 2}, {"Test4", 3}, {"Test5", 4}
+        };
+
+        readonly string SerializationString =
+            "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfKeyValueOfstringint xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" z:Id=\"1\" z:Size=\"5\" xmlns:z=\"http://schemas.microsoft.com/2003/10/Serialization/\" xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">\r\n  <KeyValueOfstringint>\r\n    <Key z:Id=\"2\">Test1</Key>\r\n    <Value>0</Value>\r\n  </KeyValueOfstringint>\r\n  <KeyValueOfstringint>\r\n    <Key z:Id=\"3\">Test2</Key>\r\n    <Value>1</Value>\r\n  </KeyValueOfstringint>\r\n  <KeyValueOfstringint>\r\n    <Key z:Id=\"4\">Test3</Key>\r\n    <Value>2</Value>\r\n  </KeyValueOfstringint>\r\n  <KeyValueOfstringint>\r\n    <Key z:Id=\"5\">Test4</Key>\r\n    <Value>3</Value>\r\n  </KeyValueOfstringint>\r\n  <KeyValueOfstringint>\r\n    <Key z:Id=\"6\">Test5</Key>\r\n    <Value>4</Value>\r\n  </KeyValueOfstringint>\r\n</ArrayOfKeyValueOfstringint>";
+    }
+}
---
+++ Source/SharpLearning.InputOutput/Serialization/GenericXmlDataContractSerializer.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace SharpLearning.InputOutput.Serialization
+{
+    /// <summary>
+    /// Generic xml serializer using DataContractSerializer
+    /// </summary>
+    public static class GenericXmlDataContractSerializer
+    {
+        /// <summary>
+        /// Serialize data to the provided writer
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="writer"></param>
+        public static void Serialize<T>(T data, Func<TextWriter> writer)
+        {
+            var settings = new XmlWriterSettings { Indent = true };
+
+            using (var stream = writer())
+            using (var xmlWriter = XmlWriter.Create(stream, settings))
+            {
+                var serializer = new DataContractSerializer(typeof(T), null, int.MaxValue, 
+                    false, true, null, new GenericResolver());
+                
+                serializer.WriteObject(xmlWriter, data);
+            }
+        }
+
+        /// <summary>
+        /// Deserialize data from the provided reader
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(Func<TextReader> reader)
+        {
+            using (var stream = reader())
+            using (var xmlReader = XmlReader.Create(stream))
+            {
+                var serializer = new DataContractSerializer(typeof(T), null, int.MaxValue, 
+                    false, true, null, new GenericResolver());
+                
+                return (T)serializer.ReadObject(xmlReader);
+            }
+        }
+
+        #region GenericResolver
+
+        /// <summary>
+        /// Internal class for generic object type resolving
+        /// </summary>
+        internal class GenericResolver : DataContractResolver
+        {
+            const string DefaultNamespace = "global";
+
+            readonly Dictionary<Type, Tuple<string, string>> m_TypeToNames;
+            readonly Dictionary<string, Dictionary<string, Type>> m_NamesToType;
+
+            public Type[] KnownTypes
+            {
+                get
+                {
+                    return m_TypeToNames.Keys.ToArray();
+                }
+            }
+
+            public GenericResolver()
+                : this(ReflectTypes())
+            { }
+
+            public GenericResolver(Type[] typesToResolve)
+            {
+                m_TypeToNames = new Dictionary<Type, Tuple<string, string>>();
+                m_NamesToType = new Dictionary<string, Dictionary<string, Type>>();
+
+                foreach (Type type in typesToResolve)
+                {
+                    string typeNamespace = GetNamespace(type);
+                    string typeName = GetName(type);
+
+                    m_TypeToNames[type] = new Tuple<string, string>(typeNamespace, typeName);
+
+                    if (m_NamesToType.ContainsKey(typeNamespace) == false)
+                    {
+                        m_NamesToType[typeNamespace] = new Dictionary<string, Type>();
+                    }
+                    m_NamesToType[typeNamespace][typeName] = type;
+                }
+            }
+
+            public static GenericResolver Merge(GenericResolver resolver1, GenericResolver resolver2)
+            {
+                if (resolver1 == null)
+                {
+                    return resolver2;
+                }
+                if (resolver2 == null)
+                {
+                    return resolver1;
+                }
+                List<Type> types = new List<Type>();
+                types.AddRange(resolver1.KnownTypes);
+                types.AddRange(resolver2.KnownTypes);
+
+                return new GenericResolver(types.ToArray());
+            }
+
+            static string GetNamespace(Type type)
+            {
+                return type.Namespace ?? DefaultNamespace;
+            }
+
+            static string GetName(Type type)
+            {
+                return type.Name;
+            }
+
+            public override Type ResolveName(string typeName, string typeNamespace, Type declaredType, DataContractResolver knownTypeResolver)
+            {
+                if (m_NamesToType.ContainsKey(typeNamespace))
+                {
+                    if (m_NamesToType[typeNamespace].ContainsKey(typeName))
+                    {
+                        return m_NamesToType[typeNamespace][typeName];
+                    }
+                }
+                return knownTypeResolver.ResolveName(typeName, typeNamespace, declaredType, null);
+            }
+
+            public override bool TryResolveType(Type type, Type declaredType, DataContractResolver knownTypeResolver, out XmlDictionaryString typeName, out XmlDictionaryString typeNamespace)
+            {
+                if (m_TypeToNames.ContainsKey(type))
+                {
+                    XmlDictionary dictionary = new XmlDictionary();
+                    typeNamespace = dictionary.Add(m_TypeToNames[type].Item1);
+                    typeName = dictionary.Add(m_TypeToNames[type].Item2);
+
+                    return true;
+                }
+                else
+                {
+                    return knownTypeResolver.TryResolveType(type, declaredType, null, out typeName, out typeNamespace);
+                }
+            }
+
+            static Type[] ReflectTypes()
+            {
+                Assembly[] assemblyReferecnes = AppDomain.CurrentDomain.GetAssemblies();
+
+                List<Type> types = new List<Type>();
+
+                foreach (Assembly assembly in assemblyReferecnes)
+                {
+                    Type[] typesInReferencedAssembly = GetTypes(assembly);
+                    types.AddRange(typesInReferencedAssembly);
+                }
+
+                return types.ToArray();
+            }
+
+
+            static Type[] GetTypes(Assembly assembly, bool publicOnly = true)
+            {
+                Type[] allTypes = assembly.GetTypes();
+
+                List<Type> types = new List<Type>();
+
+                foreach (Type type in allTypes)
+                {
+                    if (type.IsEnum == false &&
+                       type.IsInterface == false &&
+                       type.IsGenericTypeDefinition == false)
+                    {
+                        if (publicOnly == true && type.IsPublic == false)
+                        {
+                            if (type.IsNested == false)
+                            {
+                                continue;
+                            }
+                            if (type.IsNestedPrivate == true)
+                            {
+                                continue;
+                            }
+                        }
+                        types.Add(type);
+                    }
+                }
+                return types.ToArray();
+            }
+        }
+
+        #endregion
+    }
+}
