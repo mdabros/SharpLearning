2f05dc09262a439e886fc8161899a857ca786acc
Mads Dabros <m_dabros@hotmail.com>
2015-02-01 13:14:43 +0100

    SharpLearning.CrossValidation.Test 0.16.6.4: Updated NoShuffleCrossValidation tests.

--- Source/SharpLearning.CrossValidation.Test/CrossValidators/NoShuffleCrossValidationTest.cs
+++ Source/SharpLearning.CrossValidation.Test/CrossValidators/NoShuffleCrossValidationTest.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpLearning.Containers.Matrices;
 using SharpLearning.CrossValidation.Test;
+using SharpLearning.CrossValidation.Test.Properties;
+using SharpLearning.DecisionTrees.Learners;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.Metrics.Regression;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace SharpLearning.CrossValidation.CrossValidators.Test
@@ -12,19 +17,15 @@ namespace SharpLearning.CrossValidation.CrossValidators.Test
         [TestMethod]
         public void NoShuffleCrossValidation_CrossValidate_Folds_2()
         {
-            var actual = AssertCrossValidation(2);
-            var expected = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            CollectionAssert.AreEqual(expected, actual);
+            var actual = CrossValidate(2);
+            Assert.AreEqual(0.08399278971163, actual, 0.001);
         }
 
         [TestMethod]
         public void NoShuffleCrossValidation_CrossValidate_Folds_10()
         {
-            var actual = AssertCrossValidation(10);
-            var expected = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            CollectionAssert.AreEqual(expected, actual);
+            var actual = CrossValidate(10);
+            Assert.AreEqual(0.069356782107075, actual, 0.001);
         }
 
         [TestMethod]
@@ -43,6 +44,20 @@ namespace SharpLearning.CrossValidation.CrossValidators.Test
             var sut = new NoShuffleCrossValidation<double>(folds);
             var actual = sut.CrossValidate(new CrossValidationTestLearner(indices), observations, targets);
             return actual;
+        }
+
+        double CrossValidate(int folds)
+        {
+            var targetName = "T";
+            var parser = new CsvParser(() => new StringReader(Resources.DecisionTreeData));
+            var observations = parser.EnumerateRows(v => !v.Contains(targetName)).ToF64Matrix();
+            var targets = parser.EnumerateRows(targetName).ToF64Vector();
+
+            var sut = new NoShuffleCrossValidation<double>(folds);
+            var predictions = sut.CrossValidate(new RegressionDecisionTreeLearner(), observations, targets);
+            var metric = new MeanSquaredErrorRegressionMetric();
+
+            return metric.Error(targets, predictions);
         }
     }
 }
