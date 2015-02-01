a89136a362366993e964988fd05233dd87e6df94
Mads Dabros <m_dabros@hotmail.com>
2015-02-01 13:35:31 +0100

    SharpLearning.CrossValidation.Test 0.16.6.9: Updated NoShuffleBiasVarianceLearningCurvesCalculator test

--- Source/SharpLearning.CrossValidation.Test/BiasVarianceAnalysis/NoShuffleBiasVarianceLearningCurvesCalculatorTest.cs
+++ Source/SharpLearning.CrossValidation.Test/BiasVarianceAnalysis/NoShuffleBiasVarianceLearningCurvesCalculatorTest.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpLearning.Containers.Matrices;
 using SharpLearning.CrossValidation.BiasVarianceAnalysis;
+using SharpLearning.CrossValidation.Test.Properties;
+using SharpLearning.DecisionTrees.Learners;
+using SharpLearning.InputOutput.Csv;
 using System.Collections.Generic;
+using System.IO;
 
 namespace SharpLearning.CrossValidation.Test.BiasVarianceAnalysis
 {
@@ -14,15 +18,16 @@ namespace SharpLearning.CrossValidation.Test.BiasVarianceAnalysis
             var sut = new NoShuffleBiasVarianceLearningCurvesCalculator<double>(new CrossValidationTestMetric(), 
                 new double[] { 0.2, 0.8 }, 0.8 );
 
-            var observations = new F64Matrix(10, 10);
-            var targets = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var indices = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var targetName = "T";
+            var parser = new CsvParser(() => new StringReader(Resources.DecisionTreeData));
+            var observations = parser.EnumerateRows(v => !v.Contains(targetName)).ToF64Matrix();
+            var targets = parser.EnumerateRows(targetName).ToF64Vector();
 
-            var actual = sut.Calculate(() => new CrossValidationTestLearner(indices),
+            var actual = sut.Calculate(() => new RegressionDecisionTreeLearner(),
                 observations, targets);
 
-            var expected = new List<BiasVarianceLearningCurvePoint>() { new BiasVarianceLearningCurvePoint(1, 1, 36), 
-                new BiasVarianceLearningCurvePoint(6, 25.333333333333332, 0)};
+            var expected = new List<BiasVarianceLearningCurvePoint>() { new BiasVarianceLearningCurvePoint(32, 0, 0.12874833873980004), 
+                new BiasVarianceLearningCurvePoint(128, 0.0, 0.067720786718774989)};
             
             CollectionAssert.AreEqual(expected, actual);
         }
