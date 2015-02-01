5b25009cef71c95649c2ecce74a2c953df74be4c
Mads Dabros <m_dabros@hotmail.com>
2015-02-01 13:11:56 +0100

    SharpLearning.CrossValidation 0.16.6.3: Updated CrossValidation test to use real learner and data. This should provide a more real scenario and catch more potential bugs.

--- Source/SharpLearning.CrossValidation.Test/CrossValidators/CrossValidationTest.cs
+++ Source/SharpLearning.CrossValidation.Test/CrossValidators/CrossValidationTest.cs
@@ -2,7 +2,12 @@
 using SharpLearning.Containers.Matrices;
 using SharpLearning.CrossValidation.Shufflers;
 using SharpLearning.CrossValidation.Test;
+using SharpLearning.CrossValidation.Test.Properties;
+using SharpLearning.DecisionTrees.Learners;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.Metrics.Regression;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace SharpLearning.CrossValidation.CrossValidators.Test
@@ -13,37 +18,36 @@ namespace SharpLearning.CrossValidation.CrossValidators.Test
         [TestMethod]
         public void CrossValidation_CrossValidate_Folds_2()
         {
-            var actual = AssertCrossValidation(2);
-            var expected = new double[] { 2, 7, 5, 3, 4, 9, 8, 1, 6, 0 };
-
-            CollectionAssert.AreEqual(expected, actual);
+            var actual = CrossValidate(2);
+            Assert.AreEqual(0.090240740378955, actual, 0.001);
         }
 
         [TestMethod]
         public void CrossValidation_CrossValidate_Folds_10()
         {
-            var actual = AssertCrossValidation(10);
-            var expected = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            CollectionAssert.AreEqual(expected, actual);
+            var actual = CrossValidate(10);
+            Assert.AreEqual(0.07200934424307, actual, 0.001);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void CrossValidation_CrossValidate_Too_Many_Folds()
         {
-            AssertCrossValidation(20);
+            CrossValidate(2000);
         }
 
-        double[] AssertCrossValidation(int folds)
+        double CrossValidate(int folds)
         {
-            var observations = new F64Matrix(10, 10);
-            var targets = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var indices = Enumerable.Range(0, targets.Length).ToArray();
+            var targetName = "T";
+            var parser = new CsvParser(() => new StringReader(Resources.DecisionTreeData));
+            var observations = parser.EnumerateRows(v => !v.Contains(targetName)).ToF64Matrix();
+            var targets = parser.EnumerateRows(targetName).ToF64Vector();
 
             var sut = new CrossValidation<double>(new RandomCrossValidationShuffler<double>(42), folds);
-            var actual = sut.CrossValidate(new CrossValidationTestLearner(indices), observations, targets);
-            return actual;
+            var predictions = sut.CrossValidate(new RegressionDecisionTreeLearner(), observations, targets);
+            var metric = new MeanSquaredErrorRegressionMetric();
+
+            return metric.Error(targets, predictions);
         }
     }
 }
