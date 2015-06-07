31b7121fec0ef12c3b02916a0b94ca39932b7337
Mads Dabros <m_dabros@hotmail.com>
2015-06-07 19:39:50 +0200

    SharpLearning.GradientBoost.Test 0.17.22.6: Added GBMSquaredLossTest

---
+++ Source/SharpLearning.GradientBoost.Test/GBM/LossFunctions/GBMSquaredLossTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using SharpLearning.GradientBoost.GBM;
+using System.Diagnostics;
+
+namespace SharpLearning.GradientBoost.Test.GBM.LossFunctions
+{
+    [TestClass]
+    public class GBMSquaredLossTest
+    {
+        [TestMethod]
+        public void GBMSquaredLoss_InitializeLoss()
+        {
+            var targets = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sut = new GBMSquaredLoss();
+
+            var actual = sut.InitialLoss(targets, targets.Select(t => true).ToArray());
+            Assert.AreEqual(5.0, actual);
+        }
+
+        [TestMethod]
+        public void GBMSquaredLoss_UpdateResiduals()
+        {
+            var targets = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var predictions = new double[] { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };
+            var actual = new double[targets.Length];
+            var sut = new GBMSquaredLoss();
+
+            sut.UpdateResiduals(targets, predictions, actual, targets.Select(t => true).ToArray());
+
+            var expected = new double[] { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GBMSquaredLoss_UpdateResiduals_Indexed()
+        {
+            var targets = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var predictions = new double[] { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };
+            var actual = new double[targets.Length];
+            var inSample = new bool[] { true, false, true, false, true, false, true, false, true };
+            var sut = new GBMSquaredLoss();
+
+            sut.UpdateResiduals(targets, predictions, actual, inSample);
+
+            var expected = new double[] { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GBMSquaredLoss_UpdatedLeafValue()
+        {
+            var targets = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var predictions = new double[] { 2.0, 1.0, 4.0, 3.0, 4.0, 5.0, 8.0, 9.0, 1.0 };
+            var inSample = new bool[] { true, true, true, true, true, true, true, true, true };
+            var sut = new GBMSquaredLoss();
+
+            var actual = sut.UpdatedLeafValue(0.0, targets, predictions, inSample);
+            Assert.AreEqual(0.0, actual, 0.001);
+        }
+
+        [TestMethod]
+        public void GBMSquaredLoss_UpdatedLeafValue_Indexed()
+        {
+            var targets = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var predictions = new double[] { 2.0, 1.0, 4.0, 3.0, 4.0, 5.0, 8.0, 9.0, 1.0 };
+            var inSample = new bool[] { true, false, true, false, true, false, true, false, true };
+            var sut = new GBMSquaredLoss();
+
+            var actual = sut.UpdatedLeafValue(0.0, targets, predictions, inSample);
+            Assert.AreEqual(0.0, actual, 0.001);
+        }
+
+        [TestMethod]
+        public void GBMSquaredLoss_UpdateLeafValues()
+        {
+            var sut = new GBMSquaredLoss();
+            Assert.IsFalse(sut.UpdateLeafValues());
+        }
+    }
+}
