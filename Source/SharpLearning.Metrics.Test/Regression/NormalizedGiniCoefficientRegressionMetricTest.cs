28fbe91c30d4b245647b5574db7b48eb899c89d4
Mads Dabros <m_dabros@hotmail.com>
2015-07-08 23:51:06 +0200

    SharpLearning.Metrics 0.18.2.0: Fixed NormalizedGiniCoefficientRegressionMetric

--- Source/SharpLearning.Metrics.Test/Regression/NormalizedGiniCoefficientRegressionMetricTest.cs
+++ Source/SharpLearning.Metrics.Test/Regression/NormalizedGiniCoefficientRegressionMetricTest.cs
@@ -11,11 +11,11 @@ namespace SharpLearning.Metrics.Test.Regression
         public void MeanAbsolutErrorRegressionMetric_Error()
         {
             var targets = new double[] { 1.0, 2.3, 3.1, 4.4, 5.8 };
-            var predictions = new double[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            var predictions = new double[] { 1.0, 2.0, 3.0, 1000.0, 5.0 };
             var sut = new NormalizedGiniCoefficientRegressionMetric();
 
             var actual = sut.Error(targets, predictions);
-            Assert.AreEqual(0.05413105413105368, actual, 0.00001);
+            Assert.AreEqual(0.11965811965811968, actual, 0.00001);
         }
 
         [TestMethod]
--- Source/SharpLearning.Metrics/Regression/NormalizedGiniCoefficientRegressionMetric.cs
+++ Source/SharpLearning.Metrics/Regression/NormalizedGiniCoefficientRegressionMetric.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharpLearning.Metrics.Regression
 {
@@ -21,24 +18,52 @@ namespace SharpLearning.Metrics.Regression
         /// <returns></returns>
         public double Error(double[] target, double[] predicted)
         {
-            return 1.0 - GiniCoefficient(predicted) / GiniCoefficient(target);
+            return 1.0 - GiniCoefficient(target, predicted) / GiniCoefficient(target, target);
         }
 
-        double GiniCoefficient(double[] values)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="predicted"></param>
+        /// <returns></returns>
+        double GiniCoefficient(double[] target, double[] predicted)
         {
-            var ordered = values.OrderBy(v => v).ToArray();
-            var height = 0.0;
-            var area = 0.0;
+            var df = predicted.Zip(target, (p, t) => new { Prediction = p, Target = t }).ToArray();
+            df = df.OrderByDescending(p => p.Prediction).ToArray();
+            
+            var orderedPredicted = df.Select(p => p.Prediction).ToArray();
+            var orderedTargets = df.Select(p => p.Target).ToArray();
 
-            foreach (var value in ordered)
+            var rand = new List<double>();
+            for (int i = 0; i < df.Length; i++)
             {
-                height += value;
-                area += height - value / 2.0;
+                rand.Add(((double)i + 1.0) / (double)df.Length);
             }
 
-            var fairArea = height * (double)values.Length / 2.0;
+            var totalPos = orderedTargets.Sum();
+            var comPos = new List<double> { orderedTargets.First() };
 
-            return (fairArea - area) / fairArea;
+            for (int i = 1; i < df.Length; i++)
+            {
+                comPos.Add(comPos[i - 1] + orderedTargets[i]);
+            }
+
+            var lorentz = new List<double>();
+            foreach (var value in comPos)
+            {
+                lorentz.Add(value / totalPos);
+            }
+
+            var gini = new List<double>();
+            for (int i = 0; i < df.Length; i++)
+            {
+                gini.Add(lorentz[i] - rand[i]);
+            }
+
+            var giniCoef = gini.Sum();
+
+            return giniCoef;
         }
     }
 }
