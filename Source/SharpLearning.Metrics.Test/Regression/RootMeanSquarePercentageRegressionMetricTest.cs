e67b963bca9b82fdeed5052923ed05f7731bb0de
Mads Dabros <m_dabros@hotmail.com>
2015-10-22 22:15:58 +0200

    SharpLearning.Metrics 0.19.8.0: Added RootMeanSquarePercentageRegressionMetric

---
+++ Source/SharpLearning.Metrics.Test/Regression/RootMeanSquarePercentageRegressionMetricTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Metrics.Regression;
+
+namespace SharpLearning.Metrics.Test.Regression
+{
+    [TestClass]
+    public class RootMeanSquarePercentageRegressionMetricTest
+    {
+        [TestMethod]
+        public void RootMeanSquarePercentageRegressionMetricTest_Error()
+        {
+            var targets = new double[] { 1.0, 2.3, 3.1, 4.4, 5.8 };
+            var predictions = new double[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            var sut = new RootMeanSquarePercentageRegressionMetric();
+            
+            var result = sut.Error(targets, predictions);
+            Assert.AreEqual(0.0952294579674858, result, 0.00001);
+        }
+
+        [TestMethod]
+        public void RootMeanSquarePercentageRegressionMetricTest_Error_All_Targets_Zero()
+        {
+            var targets = new double[] { 0.0, 0.0 };
+            var predictions = new double[] { 0.0, 0.0 };
+            var sut = new RootMeanSquarePercentageRegressionMetric();
+
+            var result = sut.Error(targets, predictions);
+            Assert.AreEqual(double.MaxValue, result);
+        }
+
+        [TestMethod]
+        public void RootMeanSquarePercentageRegressionMetricTest_Error_Zero_Error()
+        {
+            var targets = new double[] { 1.0, 1.0 };
+            var predictions = new double[] { 1.0, 1.0 };
+            var sut = new RootMeanSquarePercentageRegressionMetric();
+
+            var result = sut.Error(targets, predictions);
+            Assert.AreEqual(0.0, result);
+        }
+    }
+}
---
+++ Source/SharpLearning.Metrics/Regression/RootMeanSquarePercentageRegressionMetric.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLearning.Metrics.Regression
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class RootMeanSquarePercentageRegressionMetric : IRegressionMetric
+    {
+        /// <summary>
+        /// Calculates the root mean square percentage error between the targets and predictions e = Sqrt(sum((t - p / t)^2)/length(t))  
+        /// </summary>
+        /// <param name="targets"></param>
+        /// <param name="predictions"></param>
+        /// <returns></returns>
+        public double Error(double[] targets, double[] predictions)
+        {
+            if (targets.Length != predictions.Length) { throw new ArgumentException("targets and predictions length do not match"); }
+
+            var meanSquareError = 0.0;
+            var validEntries = 0;
+            for (int i = 0; i < targets.Length; ++i)
+            {
+                var targetValue = targets[i];
+                var estimate = predictions[i];
+
+                if (targetValue == 0.0) { continue; }
+
+                validEntries++;
+                var error = (targetValue - estimate) / targetValue;
+                meanSquareError += error * error;
+            }
+
+            if (validEntries == 0)
+            {
+                return double.MaxValue;
+            }
+
+            meanSquareError *= (1.0 / validEntries);
+
+            return Math.Sqrt(meanSquareError);
+        }
+    }
+}
