b323efabed1cdc49239f65b61ff35a47ad3480f3
Mads Dabros <m_dabros@hotmail.com>
2015-10-25 16:05:42 +0100

    Added SharpLearning.FeatureTransformations and test project

--- Source/SharpLearning.Containers/Properties/AssemblyVersion.cs
+++ Source/SharpLearning.Containers/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.19.*")]
-[assembly: AssemblyFileVersion("0.19.8.4")]
+[assembly: AssemblyVersion("0.20.*")]
+[assembly: AssemblyFileVersion("0.20.0.0")]
 // SharpLearning
