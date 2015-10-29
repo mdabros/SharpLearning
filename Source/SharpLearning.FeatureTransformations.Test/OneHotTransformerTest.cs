3f745480b43dcac6a49e09689a73c35be1c73933
Mads Dabros <m_dabros@hotmail.com>
2015-10-29 21:21:45 +0100

    SharpLearning.FeatureTransformations 0.20.2.0: Added OneHotTransformer

---
+++ Source/SharpLearning.FeatureTransformations.Test/OneHotTransformerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.InputOutput.Csv;
+using System.IO;
+
+namespace SharpLearning.FeatureTransformations.Test
+{
+    [TestClass]
+    public class OneHotTransformerTest
+    {
+        [TestMethod]
+        public void OneHotTransformer_Transform()
+        {
+            var sut = new OneHotTransformer();
+
+            var writer = new StringWriter();
+
+            new CsvParser(() => new StringReader(Input))
+            .EnumerateRows()
+            .Transform(r => sut.Transform(r, "Day", "Open"))
+            .Write(() => writer);
+
+            var actual = writer.ToString();
+
+            Assert.AreEqual(Expected, actual);
+        }
+
+        string Expected =
+@"Day;Sales;Open;Day_Monday;Day_TuesDay;Day_WednessDay;Open_Yes;Open_No
+Monday;123;Yes;1;0;0;1;0
+TuesDay;41;No;0;1;0;0;1
+WednessDay;124;Yes;0;0;1;1;0
+Monday;51;No;1;0;0;0;1
+TuesDay;12;Yes;0;1;0;1;0";
+
+        string Input =
+@"Day;Sales;Open
+Monday;123;Yes
+TuesDay;41;No
+WednessDay;124;Yes
+Monday;51;No
+TuesDay;12;Yes
+";
+    }
+}
---
+++ Source/SharpLearning.FeatureTransformations/OneHotTransformer.cs
@@ -0,0 +1,109 @@
+﻿using SharpLearning.InputOutput.Csv;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharpLearning.FeatureTransformations
+{
+    /// <summary>
+    /// Transforms the selected features with multiple values into a set of binary features.
+    /// This is refered to as one-hot encoding:
+    /// https://en.wikipedia.org/wiki/One-hot
+    /// Forexample day { monday, tuesday } becomes
+    /// day_monday { 1, 0}
+    /// day_tuesday {0, 1}
+    /// </summary>
+    public sealed class OneHotTransformer
+    {
+        readonly Dictionary<string, HashSet<string>> m_featureMap;
+
+        /// <summary>
+        /// Transforms the selected features with multiple values into a set of binary features.
+        /// This is refered to as one-hot encoding:
+        /// https://en.wikipedia.org/wiki/One-hot
+        /// Forexample day { monday, tuesday } becomes
+        /// day_monday { 1, 0}
+        /// day_tuesday {0, 1}
+        /// </summary>
+        public OneHotTransformer()
+        {
+            m_featureMap = new Dictionary<string, HashSet<string>>();
+        }
+
+        /// <summary>
+        /// Transforms the selected features with multiple values into a set of binary features.
+        /// This is refered to as one-hot encoding:
+        /// https://en.wikipedia.org/wiki/One-hot
+        /// Forexample day { monday, tuesday } becomes
+        /// day_monday { 1, 0}
+        /// day_tuesday {0, 1}
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="columnsToMap"></param>
+        /// <returns></returns>
+        public IEnumerable<CsvRow> Transform(IEnumerable<CsvRow> rows, params string[] columnsToMap)
+        {
+            if(m_featureMap.Count == 0)
+            {
+                foreach (var column in columnsToMap)
+                {
+                    m_featureMap.Add(column, new HashSet<string>());
+                }
+            }
+
+            // build map
+            BuildFeatureMap(rows, columnsToMap);
+
+            // add encoded fetures
+            var newColumnNameToIndex = NewColumnNameToIndex(rows);
+            var additionalFeatures = m_featureMap.Select(v => v.Value.Count).Sum();
+
+            foreach (var row in rows)
+            {
+                var newValues = Enumerable.Range(0, row.Values.Length + additionalFeatures).Select(v => "0").ToArray();
+
+                row.Values.CopyTo(newValues, 0);
+                foreach (var column in columnsToMap)
+                {
+                    var value = row.GetValue(column);
+                    var key = column + "_" + value;
+                    newValues[newColumnNameToIndex[key]] = "1";
+                }
+
+                yield return new CsvRow(newColumnNameToIndex, newValues);
+            }
+        }
+
+        Dictionary<string, int> NewColumnNameToIndex(IEnumerable<CsvRow> rows)
+        {
+            var newColumnNameToIndex = rows.First().ColumnNameToIndex;
+            var index = newColumnNameToIndex.Count;
+
+            foreach (var columnSet in m_featureMap)
+            {
+                var name = columnSet.Key;
+                var values = columnSet.Value;
+
+                foreach (var value in values)
+                {
+                    newColumnNameToIndex.Add(name + "_" + value, index++);
+                }
+            }
+            return newColumnNameToIndex;
+        }
+
+        void BuildFeatureMap(IEnumerable<CsvRow> rows, string[] columnsToMap)
+        {
+            foreach (var row in rows)
+            {
+                foreach (var column in columnsToMap)
+                {
+                    var value = row.GetValue(column);
+                    if (!m_featureMap[column].Contains(value))
+                    {
+                        m_featureMap[column].Add(value);
+                    }
+                }
+            }
+        }
+    }
+}
