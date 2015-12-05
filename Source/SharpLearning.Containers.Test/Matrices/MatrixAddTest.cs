c87c7afb545a0327d78039a31f11561db9c65e9b
Mads Dabros <m_dabros@hotmail.com>
2015-12-05 12:41:10 +0100

    SharpLearning.Containers 0.20. 3.14: Added MatrixAdd with AddInPlace for adding a matrix and a vector

---
+++ Source/SharpLearning.Containers.Test/Matrices/MatrixAddTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Containers.Matrices;
+using SharpLearning.Containers.Arithmetic;
+
+namespace SharpLearning.Containers.Test.Matrices
+{
+    [TestClass]
+    public class MatrixAddTest
+    {
+        [TestMethod]
+        public void MatrixAdd_AddInPlace()
+        {
+            var m = new F64Matrix(4, 3);
+            var v = new double[] { 1, 1, 1, 1 };
+
+            MatrixAdd.AddInPlace(m, v);
+
+            var expected = new F64Matrix(4, 3);
+            expected.Initialize(() => 1.0);
+
+            Assert.AreEqual(expected, m);
+        }
+    }
+}
---
+++ Source/SharpLearning.Containers/Arithmetic/MatrixAdd.cs
@@ -0,0 +1,31 @@
+﻿using SharpLearning.Containers.Matrices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLearning.Containers.Arithmetic
+{
+    public static class MatrixAdd
+    {
+        /// <summary>
+        /// Adds a matrix and vector
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="v"></param>
+        public static void AddInPlace(F64Matrix m, double[] v)
+        {
+            var rows = m.GetNumberOfRows();
+            var cols = m.GetNumberOfColumns();
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    m[j, i] = v[j];
+                }
+            }
+        }
+    }
+}
