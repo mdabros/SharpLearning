1af285eed88825aad3dfc8f650dac9a9a77ffd2b
Mads Dabros <m_dabros@hotmail.com>
2015-12-05 17:10:26 +0100

    SharpLearning.Containers 0.20.3.18: Fixed bug in Matrix Transpose. Reverted to old implementation.

--- Source/SharpLearning.Containers/Arithmetic/MatrixTranspose.cs
+++ Source/SharpLearning.Containers/Arithmetic/MatrixTranspose.cs
@@ -28,42 +28,15 @@ namespace SharpLearning.Containers.Arithmetic
         /// <returns></returns>
         public static void TransposeF64(F64Matrix matrix, F64Matrix transposed)
         {
-            cachetranpose(0, matrix.GetNumberOfRows(), 0, matrix.GetNumberOfColumns(), 
-                matrix.GetFeatureArray(), transposed.GetFeatureArray());
-        }
+            var cols = matrix.GetNumberOfColumns();
+            var rows = matrix.GetNumberOfRows();
 
-        /// <summary>
-        /// Cache oblivious matrix transpose from stack overflow:
-        /// http://stackoverflow.com/questions/5200338/a-cache-efficient-matrix-transpose-program
-        /// </summary>
-        /// <param name="rb"></param>
-        /// <param name="re"></param>
-        /// <param name="cb"></param>
-        /// <param name="ce"></param>
-        /// <param name="m"></param>
-        /// <param name="T"></param>
-        static void cachetranpose(int rb, int re, int cb, int ce, double[] m, double[] T)
-        {
-            int r = re - rb, c = ce - cb;
-            if (r <= 16 && c <= 16)
+            for (int i = 0; i < rows; i++)
             {
-                for (int i = rb; i < re; i++)
+                for (int j = 0; j < cols; j++)
                 {
-                    for (int j = cb; j < ce; j++)
-                    {
-                        T[j * re + i] = m[i * ce + j];
-                    }
+                    transposed.SetItemAt(j, i, matrix.GetItemAt(i, j));
                 }
-            }
-            else if (r >= c)
-            {
-                cachetranpose(rb, rb + (r / 2), cb, ce, m, T);
-                cachetranpose(rb + (r / 2), re, cb, ce, m, T);
-            }
-            else
-            {
-                cachetranpose(rb, re, cb, cb + (c / 2), m, T);
-                cachetranpose(rb, re, cb + (c / 2), ce, m, T);
             }
         }
 
