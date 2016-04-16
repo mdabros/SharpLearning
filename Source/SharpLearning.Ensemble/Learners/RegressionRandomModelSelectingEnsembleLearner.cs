6b4b7fb621331093af0b768dd8ca89103e358b40
Mads Dabros <m_dabros@hotmail.com>
2016-04-16 16:50:16 +0200

    SharpLearning.Ensemble 0.21.11.0: Added RandomRegressionEnsembleSelection and RegressionRandomModelSelectingEnsembleLearner

---
+++ Source/SharpLearning.Ensemble/Learners/RegressionRandomModelSelectingEnsembleLearner.cs
@@ -0,0 +1,54 @@
+﻿using SharpLearning.Common.Interfaces;
+using SharpLearning.CrossValidation.CrossValidators;
+using SharpLearning.Ensemble.EnsembleSelectors;
+using SharpLearning.Ensemble.Strategies;
+using SharpLearning.Metrics.Regression;
+
+namespace SharpLearning.Ensemble.Learners
+{
+    /// <summary>
+    /// Regression model selecting EnsembleLearner. 
+    /// Trains several models and selects the best subset of models for the ensemble using iterative random selection.
+    /// The selection of the best set of models is based on cross validation.
+    /// http://www.cs.cornell.edu/~alexn/papers/shotgun.icml04.revised.rev2.pdf
+    /// </summary>
+    public sealed class RegressionRandomModelSelectingEnsembleLearner : RegressionModelSelectingEnsembleLearner
+    {
+        /// <summary>
+        /// Regression model selecting EnsembleLearner. 
+        /// Trains several models and selects the best subset of models for the ensemble using iterative random selection.
+        /// The selection of the best set of models is based on cross validation. 
+        /// Default is 5-fold RandomCrossValidation and minimization of mean square error and mean is used to combine the models.
+        /// http://www.cs.cornell.edu/~alexn/papers/shotgun.icml04.revised.rev2.pdf
+        /// </summary>
+        /// <param name="learners">Learners in the ensemble</param>
+        /// <param name="numberOfModelsToSelect">Number of models to select</param>
+        /// <param name="iterations">Number of iterations to random select model combinations.</param> 
+        public RegressionRandomModelSelectingEnsembleLearner(IIndexedLearner<double>[] learners, int numberOfModelsToSelect, int iterations=50)
+            : this(learners, new RandomCrossValidation<double>(5, 42), new MeanRegressionEnsembleStrategy(), new MeanSquaredErrorRegressionMetric(),
+                  numberOfModelsToSelect)
+        {
+        }
+
+
+        /// <summary>
+        /// Regression model selecting EnsembleLearner. 
+        /// Trains several models and selects the best subset of models for the ensemble using iterative random selection.
+        /// The selection of the best set of models is based on cross validation. 
+        /// http://www.cs.cornell.edu/~alexn/papers/shotgun.icml04.revised.rev2.pdf
+        /// </summary>
+        /// <param name="learners">Learners in the ensemble</param>
+        /// <param name="crossValidation">Cross validation method</param>
+        /// <param name="ensembleStrategy">Strategy for ensembling models</param>
+        /// <param name="metric">Metric to minimize</param>
+        /// <param name="numberOfModelsToSelect">Number of models to select</param>
+        /// <param name="iterations">Number of iterations to random select model combinations.</param> 
+        /// <param name="selectWithReplacement">If true the same model can be selected multiple times.</param>
+        /// <param name="seed"></param>
+        public RegressionRandomModelSelectingEnsembleLearner(IIndexedLearner<double>[] learners, ICrossValidation<double> crossValidation,
+            IRegressionEnsembleStrategy ensembleStrategy, IMetric<double, double> metric, int numberOfModelsToSelect, int iterations = 50, bool selectWithReplacement = true, int seed = 42)
+            : base(learners, crossValidation, ensembleStrategy, new RandomRegressionEnsembleSelection(metric, ensembleStrategy, numberOfModelsToSelect, iterations, selectWithReplacement, seed))
+        {
+        }
+    }
+}
