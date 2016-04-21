47ec7d15211a07f90f68671164a793b3de517880
Mads Dabros <m_dabros@hotmail.com>
2016-04-21 12:56:55 +0200

    SharpLearning.Optimization 0.21.17.0: Added ParticleSwarmOptimizer

---
+++ Source/SharpLearning.Optimization.Test/ParticleSwarmOptimizerTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SharpLearning.Optimization.Test
+{
+    [TestClass]
+    public class ParticleSwarmOptimizerTest
+    {
+        [TestMethod]
+        public void ParticleSwarmOptimizer_OptimizeBest()
+        {
+            var parameters = new double[][]
+            {
+                new double[] { -10.0, 10.0 },
+                new double[] { -10.0, 10.0 },
+                new double[] { -10.0, 10.0 }
+            };
+            var sut = new ParticleSwarmOptimizer(parameters, 100);
+            var actual = sut.OptimizeBest(Minimize);
+
+            Assert.AreEqual(actual.Error, -0.38111188515223809, 0.0000001);
+            Assert.AreEqual(actual.ParameterSet.Length, 3);
+
+            Assert.AreEqual(actual.ParameterSet[0], 10, 0.0000001);
+            Assert.AreEqual(actual.ParameterSet[1], 0.003223641308240488, 0.0000001);
+            Assert.AreEqual(actual.ParameterSet[2], 0.42745032466778454, 0.0000001);
+        }
+
+        [TestMethod]
+        public void ParticleSwarmOptimizer_Optimize()
+        {
+            var parameters = new double[][] { new double[] { 0.0, 100.0 } };
+            var sut = new ParticleSwarmOptimizer(parameters, 100);
+            var results = sut.Optimize(Minimize2);
+            var actual = new OptimizerResult[] { results.First(), results.Last() };
+
+            var expected = new OptimizerResult[]
+            {
+                new OptimizerResult(new double[] { 37.635959742891046 }, 109.58839344747747),
+                new OptimizerResult(new double[] { 38.891904333594624 }, 166.38611320480345)
+            };
+
+            Assert.AreEqual(expected.First().Error, actual.First().Error, 0.0001);
+            Assert.AreEqual(expected.First().ParameterSet.First(), actual.First().ParameterSet.First(), 0.0001);
+
+            Assert.AreEqual(expected.Last().Error, actual.Last().Error, 0.0001);
+            Assert.AreEqual(expected.Last().ParameterSet.First(), actual.Last().ParameterSet.First(), 0.0001);
+        }
+
+
+        OptimizerResult Minimize(double[] x)
+        {
+            return new OptimizerResult(x, Math.Sin(x[0]) * Math.Cos(x[1]) * (1.0 / (Math.Abs(x[2]) + 1)));
+        }
+
+        OptimizerResult Minimize2(double[] parameters)
+        {
+            var heights = new double[] { 1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83 };
+            var weights = new double[] { 52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46 };
+
+            var cost = 0.0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                cost += (parameters[0] * heights[i] - weights[i]) * (parameters[0] * heights[i] - weights[i]);
+            }
+
+            return new OptimizerResult(parameters, cost);
+        }
+    }
+}
---
+++ Source/SharpLearning.Optimization/ParticleSwarmOptimizer.cs
@@ -0,0 +1,179 @@
+﻿using SharpLearning.Containers.Arithmetic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SharpLearning.Optimization
+{
+    /// <summary>
+    /// Particle Swarm optimizer (PSO). PSO is initialized with a group of random particles
+    /// and then searches for optima by updating generations. In every iteration, each particle is updated by following two "best" values. 
+    /// The first one is the best solution found by the specific particle so far. 
+    /// The other "best" value is the global best value obtained by any particle in the population so far.
+    /// http://www.swarmintelligence.org/tutorials.php
+    /// https://en.wikipedia.org/wiki/Particle_swarm_optimization
+    /// </summary>
+    public sealed class ParticleSwarmOptimizer : IOptimizer
+    {
+        readonly double[][] m_parameters;
+        readonly int m_maxIterations;
+        readonly int m_numberOfParticles;
+        readonly double m_c1;
+        readonly double m_c2;
+        readonly Random m_random;
+
+        /// <summary>
+        /// Particle Swarm optimizer (PSO). PSO is initialized with a group of random particles
+        /// and then searches for optima by updating generations. In every iteration, each particle is updated by following two "best" values. 
+        /// The first one is the best solution found by the specific particle so far. 
+        /// The other "best" value is the global best value obtained by any particle in the population so far.
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="maxIterations"></param>
+        /// <param name="numberOfParticles"></param>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <param name="seed"></param>
+        public ParticleSwarmOptimizer(double[][] parameters, int maxIterations, int numberOfParticles = 10, double c1 = 2, double c2 = 2, int seed = 42)
+        {
+            m_parameters = parameters;
+            m_maxIterations = maxIterations;
+            m_numberOfParticles = numberOfParticles;
+            m_c1 = c1;
+            m_c2 = c2;
+            
+            m_random = new Random(seed);
+        }
+
+        /// <summary>
+        /// Optimization using swarm optimization.
+        /// Returns the result which best minimises the provided function.
+        /// </summary>
+        /// <param name="functionToMinimize"></param>
+        /// <returns></returns>
+        public OptimizerResult OptimizeBest(Func<double[], OptimizerResult> functionToMinimize)
+        {
+            return Optimize(functionToMinimize).First();
+        }
+
+        /// <summary>
+        /// Optimization using swarm optimization.
+        /// Returns the final results ordered from best to worst (minimized).
+        /// </summary>
+        /// <param name="functionToMinimize"></param>
+        /// <returns></returns>
+        public OptimizerResult[] Optimize(Func<double[], OptimizerResult> functionToMinimize)
+        {
+            var particles = new double[m_numberOfParticles][];
+
+            var particleVelocities = Enumerable.Range(0, m_numberOfParticles)
+                .Select(p => new double[m_parameters.Length])
+                .ToArray();
+
+            // initialize max and min velocities
+            var maxParticleVelocities = new double[m_parameters.Length];
+            var minParticleVelocities = new double[m_parameters.Length];
+            for (int i = 0; i < m_parameters.Length; i++)
+            {
+                maxParticleVelocities[i] = Math.Abs(m_parameters[i].Max() - m_parameters[i].Min());
+                minParticleVelocities[i] = -maxParticleVelocities[i];
+            }
+
+            // initialize max and min parameter bounds
+            var maxParameters = new double[m_parameters.Length];
+            var minParameters = new double[m_parameters.Length];
+            for (int i = 0; i < m_parameters.Length; i++)
+            {
+                maxParameters[i] = m_parameters[i].Max();
+                minParameters[i] = m_parameters[i].Min();
+            }
+
+            var pBest = Enumerable.Range(0, m_numberOfParticles)
+                .Select(p => new double[m_parameters.Length])
+                .ToArray();
+
+            var pBestScores = Enumerable.Range(0, m_numberOfParticles)
+                .Select(p => double.MaxValue)
+                .ToArray();
+
+            var gBest = new OptimizerResult(new double[m_parameters.Length], double.MaxValue);
+            
+            // random initialize particles
+            for (int i = 0; i < m_numberOfParticles; i++)
+            {
+                particles[i] = CreateParticle();
+            }
+
+            // iterate for find best
+            for (int iterations = 0; iterations < m_maxIterations; iterations++)
+            {
+                for (int i = 0; i < m_numberOfParticles; i++)
+                {
+                    var result = functionToMinimize(particles[i]);
+                    if(result.Error < pBestScores[i])
+                    {
+                        pBest[i] = result.ParameterSet;
+                        pBestScores[i] = result.Error;
+                    }
+
+                    if(result.Error < gBest.Error)
+                    {
+                        gBest = new OptimizerResult(result.ParameterSet.ToArray(), result.Error);
+                        Trace.WriteLine(gBest.Error);
+                    }
+                }
+
+                for (int i = 0; i < m_numberOfParticles; i++)
+                {
+                    //v[] = v[] + c1 * rand() * (pbest[] - present[]) + c2 * rand() * (gbest[] - present[])
+                    particleVelocities[i] = particleVelocities[i].Add(pBest[i].Subtract(particles[i]).Multiply(m_c1 * m_random.NextDouble())
+                        .Add(gBest.ParameterSet.Subtract(particles[i]).Multiply(m_c2 * m_random.NextDouble())));
+
+                    BoundCheck(particleVelocities[i], maxParticleVelocities, minParticleVelocities);
+
+                    //present[] = persent[] + v[]
+                    particles[i] = particles[i].Add(particleVelocities[i]);
+                    BoundCheck(particles[i], maxParameters, minParameters);
+
+                }
+            }
+
+            var results = new List<OptimizerResult>();
+            results.Add(gBest);
+
+            for (int i = 0; i < m_numberOfParticles; i++)
+            {
+                results.Add(new OptimizerResult(pBest[i], pBestScores[i]));
+            }                            
+
+            return results.Where(v => !double.IsNaN(v.Error)).OrderBy(r => r.Error).ToArray();
+        }
+
+        void BoundCheck(double[] newValues, double[] maxValues, double[] minValues)
+        {
+            for (int i = 0; i < newValues.Length; i++)
+            {
+                newValues[i] = Math.Max(minValues[i], Math.Min(newValues[i], maxValues[i]));
+            }
+        }
+
+        double[] CreateParticle()
+        {
+            var newPoint = new double[m_parameters.Length];
+
+            for (int i = 0; i < m_parameters.Length; i++)
+            {
+                var range = m_parameters[i];
+                newPoint[i] = NewParameter(range.Min(), range.Max());
+            }
+
+            return newPoint;
+        }
+
+        double NewParameter(double min, double max)
+        {
+            return m_random.NextDouble() * (max - min) + min;
+        }
+    }
+}
