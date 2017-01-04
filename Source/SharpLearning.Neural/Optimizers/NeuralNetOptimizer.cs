bf6da486499f39bce81139ce1bd1944c0090b9c2
Mads Dabros <m_dabros@hotmail.com>
2017-01-04 13:50:15 +0100

    SharpLearning.Neural 0.23.0.1: Refactored NeuralNetOptimizer
    Former-commit-id: 724237dc2a2c04bee68fff9d6056fa6e7ca3ddad

--- Source/SharpLearning.Neural/Optimizers/NeuralNetOptimizer.cs
+++ Source/SharpLearning.Neural/Optimizers/NeuralNetOptimizer.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 
 namespace SharpLearning.Neural.Optimizers
 {
-
-
     /// <summary>
     /// Neural net optimizer for controlling the weight updates in neural net learning.
     /// uses mini-batch stochastic gradient descent. 
@@ -14,9 +12,9 @@ namespace SharpLearning.Neural.Optimizers
     /// </summary>
     public sealed class NeuralNetOptimizer
     {
-        readonly float LearningRate;
-        readonly float Momentum;
-        readonly int BatchSize;
+        readonly float m_learningRate;
+        readonly float m_momentum;
+        readonly int m_batchSize;
 
         readonly List<double[]> gsumWeights = new List<double[]>(); // last iteration gradients (used for momentum calculations)
         readonly List<double[]> xsumWeights = new List<double[]>(); // used in adam or adadelta
@@ -24,17 +22,16 @@ namespace SharpLearning.Neural.Optimizers
         readonly List<double[]> gsumBias = new List<double[]>(); // last iteration gradients (used for momentum calculations)
         readonly List<double[]> xsumBias = new List<double[]>(); // used in adam or adadelta
 
+        readonly OptimizerMethod OptimizerMethod = OptimizerMethod.Sgd;
+        readonly float m_ro = 0.95f;
+        readonly float m_eps = 1e-6f;
+        readonly float m_beta1 = 0.9f;
+        readonly float m_beta2 = 0.999f;
 
-        int IterationCounter; // iteration counter
+        readonly float m_l1Decay = 0.0f;
+        readonly float m_l2Decay = 0.0f;
 
-        OptimizerMethod OptimizerMethod = OptimizerMethod.Sgd;
-        float Ro = 0.95f;
-        float Eps = 1e-6f;
-        float Beta1 = 0.9f;
-        float Beta2 = 0.999f;
-
-        float L1Decay = 0.0f;
-        float L2Decay = 0.0f;
+        int m_iterationCounter; // iteration counter
 
         /// <summary>
         /// Neural net optimizer for controlling the weight updates in neural net learning.
@@ -62,16 +59,16 @@ namespace SharpLearning.Neural.Optimizers
             if (beta1 <= 0) { throw new ArgumentNullException("beta1 must be larger than 0. Was: " + beta1); }
             if (beta2 <= 0) { throw new ArgumentNullException("beta2 must be larger than 0. Was: " + beta2); }
 
-            LearningRate = (float)learningRate;
-            BatchSize = batchSize;
-            L1Decay = (float)l1decay;
-            L2Decay = (float)l2decay;
+            m_learningRate = (float)learningRate;
+            m_batchSize = batchSize;
+            m_l1Decay = (float)l1decay;
+            m_l2Decay = (float)l2decay;
             
             OptimizerMethod = optimizerMethod;
-            Momentum = (float)momentum;
-            Ro = (float)ro;
-            Beta1 = (float)beta1;
-            Beta2 = (float)beta2;
+            m_momentum = (float)momentum;
+            m_ro = (float)ro;
+            m_beta1 = (float)beta1;
+            m_beta2 = (float)beta2;
         }
 
         /// <summary>
@@ -80,60 +77,54 @@ namespace SharpLearning.Neural.Optimizers
         /// <param name="parametersAndGradients"></param>
         public void UpdateParameters(List<ParametersAndGradients> parametersAndGradients)
         {
-            this.IterationCounter++;
-            // initialize lists for accumulators. Will only be done once on first iteration
-            if (this.gsumWeights.Count == 0 && (this.OptimizerMethod != OptimizerMethod.Sgd || this.Momentum > 0.0))
-            {
-                // only vanilla sgd doesnt need either lists
-                // momentum needs gsum
-                // adagrad needs gsum
-                // adam and adadelta needs gsum and xsum
-                for (var i = 0; i < parametersAndGradients.Count; i++)
-                {
-                    this.gsumWeights.Add(new double[parametersAndGradients[i].Parameters.Weights.Data().Length]);
-                    this.gsumBias.Add(new double[parametersAndGradients[i].Parameters.Bias.Data().Length]);
-                    if (this.OptimizerMethod == OptimizerMethod.Adam || this.OptimizerMethod == OptimizerMethod.Adadelta)
-                    {
-                        this.xsumWeights.Add(new double[parametersAndGradients[i].Parameters.Weights.Data().Length]);
-                        this.xsumBias.Add(new double[parametersAndGradients[i].Parameters.Bias.Data().Length]);
-                    }
-                }
-            }
+            m_iterationCounter++;
+            
+            // initialize accumulators. Will only be done once on first iteration and if optimizer methods is not sgd
+            var useAccumulators = gsumWeights.Count == 0 && (OptimizerMethod != OptimizerMethod.Sgd || m_momentum > 0.0);
+            if (useAccumulators) { InitializeAccumulators(parametersAndGradients); }
 
-            // perform an update for all sets of weights
+            // perform update of all parameters
             Parallel.For(0, parametersAndGradients.Count, i => 
             {
                 var parametersAndGradient = parametersAndGradients[i];
-                // param, gradient, other options in future (custom learning rate etc)
+                
+                // extract parameters and gradients
                 var parameters = parametersAndGradient.Parameters.Weights.Data();
                 var parametersBias = parametersAndGradient.Parameters.Bias.Data();
                 var gradients = parametersAndGradient.Gradients.Weights.Data();
                 var gradientsBias = parametersAndGradient.Gradients.Bias.Data();
 
-                // learning rate for some parameters.
-                var l2DecayMul = 1.0;
-                var l1DecayMul = 1.0;
-                var l2Decay = this.L2Decay * l2DecayMul;
-                var l1Decay = this.L1Decay * l1DecayMul;
-
                 // update weights
-                UpdateParam(i, parameters, gradients, l2Decay, l1Decay, gsumWeights, xsumWeights);
+                UpdateParam(i, parameters, gradients, m_l2Decay, m_l1Decay, gsumWeights, xsumWeights);
                     
                 // Update biases
-                UpdateParam(i, parametersBias, gradientsBias, l2Decay, l1Decay, gsumBias, xsumBias);
+                UpdateParam(i, parametersBias, gradientsBias, m_l2Decay, m_l1Decay, gsumBias, xsumBias);
             });
+        }
+
+        void InitializeAccumulators(List<ParametersAndGradients> parametersAndGradients)
+        {
+            for (var i = 0; i < parametersAndGradients.Count; i++)
+            {
+                gsumWeights.Add(new double[parametersAndGradients[i].Parameters.Weights.Data().Length]);
+                gsumBias.Add(new double[parametersAndGradients[i].Parameters.Bias.Data().Length]);
+                if (OptimizerMethod == OptimizerMethod.Adam || OptimizerMethod == OptimizerMethod.Adadelta)
+                {
+                    xsumWeights.Add(new double[parametersAndGradients[i].Parameters.Weights.Data().Length]);
+                    xsumBias.Add(new double[parametersAndGradients[i].Parameters.Bias.Data().Length]);
+                }
+            }
         }
 
         private void UpdateParam(int i, float[] parameters, float[] gradients, double l2Decay, double l1Decay,
             List<double[]> gsum, List<double[]> xsum)
         {
-            var plen = parameters.Length;
-            for (var j = 0; j < plen; j++)
+            for (var j = 0; j < parameters.Length; j++)
             {
                 var l1Grad = l1Decay * (parameters[j] > 0 ? 1 : -1);
                 var l2Grad = l2Decay * (parameters[j]);
 
-                var gij = (l2Grad + l1Grad + gradients[j]) / this.BatchSize; // raw batch gradient
+                var gij = (l2Grad + l1Grad + gradients[j]) / m_batchSize; // raw batch gradient
 
                 double[] gsumi = null;
                 if (gsum.Count > 0)
@@ -147,68 +138,59 @@ namespace SharpLearning.Neural.Optimizers
                     xsumi = xsum[i];
                 }
 
-                switch (this.OptimizerMethod)
+                switch (OptimizerMethod)
                 {
                     case OptimizerMethod.Sgd:
                         {
-                            if (this.Momentum > 0.0)
+                            if (m_momentum > 0.0) // sgd + momentum
                             {
-                                // momentum update
-                                var dx = this.Momentum * gsumi[j] - this.LearningRate * gij; // step
+                                var dx = m_momentum * gsumi[j] - m_learningRate * gij; // step
                                 gsumi[j] = dx; // back this up for next iteration of momentum
                                 parameters[j] += (float)dx; // apply corrected gradient
                             }
-                            else
+                            else // standard sgd
                             {
-                                // vanilla sgd
-                                parameters[j] += (float)(-this.LearningRate * gij);
+                                parameters[j] += (float)(-m_learningRate * gij);
                             }
                         }
                         break;
                     case OptimizerMethod.Adam:
                         {
-                            // adam update
-                            gsumi[j] = gsumi[j] * this.Beta1 + (1 - this.Beta1) * gij; // update biased first moment estimate
-                            xsumi[j] = xsumi[j] * this.Beta2 + (1 - this.Beta2) * gij * gij; // update biased second moment estimate
-                            var biasCorr1 = gsumi[j] * (1 - Math.Pow(this.Beta1, this.IterationCounter)); // correct bias first moment estimate
-                            var biasCorr2 = xsumi[j] * (1 - Math.Pow(this.Beta2, this.IterationCounter)); // correct bias second moment estimate
-                            var dx = -this.LearningRate * biasCorr1 / (Math.Sqrt(biasCorr2) + this.Eps);
+                            gsumi[j] = gsumi[j] * m_beta1 + (1 - m_beta1) * gij; // update biased first moment estimate
+                            xsumi[j] = xsumi[j] * m_beta2 + (1 - m_beta2) * gij * gij; // update biased second moment estimate
+                            var biasCorr1 = gsumi[j] * (1 - Math.Pow(m_beta1, m_iterationCounter)); // correct bias first moment estimate
+                            var biasCorr2 = xsumi[j] * (1 - Math.Pow(m_beta2, m_iterationCounter)); // correct bias second moment estimate
+                            var dx = -m_learningRate * biasCorr1 / (Math.Sqrt(biasCorr2) + m_eps);
                             parameters[j] += (float)dx;
                         }
                         break;
                     case OptimizerMethod.Adagrad:
                         {
-                            // adagrad update
                             gsumi[j] = gsumi[j] + gij * gij;
-                            var dx = -this.LearningRate / Math.Sqrt(gsumi[j] + this.Eps) * gij;
+                            var dx = -m_learningRate / Math.Sqrt(gsumi[j] + m_eps) * gij;
                             parameters[j] += (float)dx;
                         }
                         break;
                     case OptimizerMethod.Adadelta:
                         {
-                            // assume adadelta if not sgd or adagrad
-                            gsumi[j] = this.Ro * gsumi[j] + (1 - this.Ro) * gij * gij;
-                            var dx = -Math.Sqrt((xsumi[j] + this.Eps) / (gsumi[j] + this.Eps)) * gij;
-                            xsumi[j] = this.Ro * xsumi[j] + (1 - this.Ro) * dx * dx; // yes, xsum lags behind gsum by 1.
+                            gsumi[j] = m_ro * gsumi[j] + (1 - m_ro) * gij * gij;
+                            var dx = -Math.Sqrt((xsumi[j] + m_eps) / (gsumi[j] + m_eps)) * gij;
+                            xsumi[j] = m_ro * xsumi[j] + (1 - m_ro) * dx * dx;
                             parameters[j] += (float)dx;
                         }
                         break;
                     case OptimizerMethod.Windowgrad:
                         {
-                            // this is adagrad but with a moving window weighted average
-                            // so the gradient is not accumulated over the entire history of the run. 
-                            // it's also referred to as Idea #1 in Zeiler paper on Adadelta. Seems reasonable to me!
-                            gsumi[j] = this.Ro * gsumi[j] + (1 - this.Ro) * gij * gij;
-                            var dx = -this.LearningRate / Math.Sqrt(gsumi[j] + this.Eps) * gij;
-                            // eps added for better conditioning
+                            gsumi[j] = m_ro * gsumi[j] + (1 - m_ro) * gij * gij;
+                            var dx = -m_learningRate / Math.Sqrt(gsumi[j] + m_eps) * gij;
                             parameters[j] += (float)dx;
                         }
                         break;
                     case OptimizerMethod.Netsterov:
                         {
                             var dx = gsumi[j];
-                            gsumi[j] = gsumi[j] * this.Momentum + this.LearningRate * gij;
-                            dx = this.Momentum * dx - (1.0 + this.Momentum) * gsumi[j];
+                            gsumi[j] = gsumi[j] * m_momentum + m_learningRate * gij;
+                            dx = m_momentum * dx - (1.0 + m_momentum) * gsumi[j];
                             parameters[j] += (float)dx;
                         }
                         break;
@@ -216,7 +198,7 @@ namespace SharpLearning.Neural.Optimizers
                         throw new ArgumentOutOfRangeException();
                 }
 
-                gradients[j] = 0.0f; // zero out gradient so that we can begin accumulating anew
+                gradients[j] = 0.0f; // zero out gradient between each iteration
             }
         }
 
@@ -226,7 +208,7 @@ namespace SharpLearning.Neural.Optimizers
         public void Reset()
         {
             // clear counter
-            this.IterationCounter = 0;
+            m_iterationCounter = 0;
             
             // clear sums
             for (int i = 0; i < gsumWeights.Count; i++)
