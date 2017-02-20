4d2a50d80cdb1fb8b16cd58a6153acf91ad527ad
mada <mada@ihfood.dk>
2017-02-20 08:53:15 +0100

    Updated AssemblyVersion

--- src/SharpLearning.Containers/Properties/AssemblyVersion.cs
+++ src/SharpLearning.Containers/Properties/AssemblyVersion.cs
@@ -10,11 +10,11 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.25.0.0")]
-[assembly: AssemblyFileVersion("0.25.0.0")]
-[assembly: AssemblyInformationalVersion("0.25.0.0")]
+[assembly: AssemblyFileVersion("0.25.0.1")]
+[assembly: AssemblyInformationalVersion("0.25.0.1")]
 // SharpLearning
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("@mdabros")]
+[assembly: AssemblyCompany("mdabros")]
 [assembly: AssemblyCopyright("Copyright © Mads Dabros 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
