e8deed755a27803b503388e2b180d3ce08d4dd26
mdabros <mdabros@users.noreply.github.com>
2017-03-07 17:20:57 +0100

    Added MaxPool dot net implentation based on mkl dnn ref

---
+++ src/SharpLearning.Neural.Test/Providers/DotNetOp/MaxPoolTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Neural.Layers;
+using System.Diagnostics;
+using SharpLearning.Neural.Initializations;
+using MathNet.Numerics.LinearAlgebra;
+using SharpLearning.Neural.Providers.DotNetOp;
+using SharpLearning.Containers.Tensors;
+using System.Linq;
+using MathNet.Numerics;
+
+namespace SharpLearning.Neural.Test.Providers.DotNetOp
+{
+    [TestClass]
+    public class MaxPoolTest
+    {
+        [TestMethod]
+        public void MaxPoolTest_Forward_Prototype_Timing()
+        {
+            const int width = 32;
+            const int height = 32;
+            const int depth = 3;
+            const int batchSize = 128;
+            const int units = 256;
+
+            int poolHeight = 2;
+            int poolWidth = 2;
+
+            int strideH = 2;
+            int strideW = 2;
+
+            int padH = 0;
+            int padW = 0;
+
+            var iterations = 10;
+
+            var timer = new Stopwatch();
+            var ellapsed = RunCurrent(iterations, timer,
+                width, height, depth, batchSize,
+                poolHeight, poolWidth, 
+                strideH, strideW, padH, padW);
+
+            Trace.WriteLine($"Current: {ellapsed}");
+
+            timer.Reset();
+            ellapsed = RunDotNet(iterations, timer,
+                width, height, depth, batchSize,
+                poolHeight, poolWidth,
+                strideH, strideW, padH, padW);
+
+            Trace.WriteLine($"DotNet: {ellapsed}");
+
+            //Assert.IsFalse(true);
+        }
+
+        double RunDotNet(int iterations, Stopwatch timer,
+            int width, int height, int depth, int batchSize,
+            int poolHeight, int poolWidth,
+            int strideH, int strideW,
+            int padH, int padW)
+        {
+            // computed
+            var outputC = depth;
+            var outputW = ConvUtils.GetFilterGridLength(width, poolWidth, strideW, padW, BorderMode.Undefined);
+            var outputH = ConvUtils.GetFilterGridLength(height, poolHeight, strideH, padH, BorderMode.Undefined);
+
+            // store switches for x,y coordinates for where the max comes from, for each output neuron
+            var fanIn = width * depth * height;
+            var fanOut = outputC * outputW * outputH;
+
+            var switches = new int[batchSize * fanIn];
+
+            var input = Tensor<float>.CreateRowMajor(batchSize, depth, height, width);
+            var output = Tensor<float>.CreateRowMajor(batchSize, outputC, outputH, outputW);
+            
+            MaxPool.Forward(input, poolHeight, poolWidth, 
+                strideH, strideW, padH, padW, switches, output);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                timer.Start();
+                MaxPool.Forward(input, poolHeight, poolWidth,
+                    strideH, strideW, padH, padW, switches, output);
+                timer.Stop();
+            }
+
+            return timer.ElapsedMilliseconds / (double)iterations;
+        }
+
+        double RunCurrent(int iterations, Stopwatch timer,
+            int width, int height, int depth, int batchSize,
+            int poolHeight, int poolWidth,
+            int strideH, int strideW,
+            int padH, int padW)
+        {
+            var sut = new MaxPool2DLayer(poolWidth, poolHeight, strideH, padW, padH);
+
+            sut.Initialize(width, height, depth, batchSize, 
+                Initialization.GlorotUniform, new Random(232));
+
+            var fanIn = width * height * depth;
+            var input = Matrix<float>.Build.Dense(batchSize, fanIn);
+
+            sut.Forward(input);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                timer.Start();
+                var actual = sut.Forward(input);
+                timer.Stop();
+            }
+
+            return timer.ElapsedMilliseconds / (double)iterations;
+        }   
+    }
+}
---
+++ src/SharpLearning.Neural/Providers/DotNetOp/MaxPool.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpLearning.Containers.Tensors;
+
+namespace SharpLearning.Neural.Providers.DotNetOp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MaxPool
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="poolHeight"></param>
+        /// <param name="poolWidth"></param>
+        /// <param name="strideH"></param>
+        /// <param name="strideW"></param>
+        /// <param name="padH"></param>
+        /// <param name="padW"></param>
+        /// <param name="switches"></param>
+        /// <param name="output"></param>
+        public static void Forward(Tensor<float> input,
+            int poolHeight, int poolWidth,
+            int strideH, int strideW,
+            int padH, int padW,
+            int[] switches,
+            Tensor<float> output)
+        {
+            var src = input;
+            var dst = output;
+
+            int MB = src.Dimensions[0];
+            int OC = src.Dimensions[1];
+            int OH = dst.Dimensions[2];
+            int OW = dst.Dimensions[3];
+
+            var dstData = dst.Data;
+            //for (int mb = 0; mb < MB; ++mb)
+            Parallel.For(0, MB, mb =>
+            {
+                var dstBOffSet = dst.DimensionOffSets[0] * mb;
+                for (int oc = 0; oc < OC; ++oc)
+                {
+                    var dstcOffSet = dstBOffSet + dst.DimensionOffSets[1] * oc;
+                    for (int oh = 0; oh < OH; ++oh)
+                    {
+                        var dsthOffSet = dstcOffSet + dst.DimensionOffSets[2] * oh;
+                        for (int ow = 0; ow < OW; ++ow)
+                        {
+                            var dstIndex = dsthOffSet + ow;
+                            dstData[dstIndex] = MaxPoolForward(input, mb, oc, oh, ow,
+                                poolHeight, poolWidth, 
+                                strideH, strideW,
+                                padH, padW, switches);
+                        }
+                    }
+                }
+            });
+        }
+
+        static float MaxPoolForward(Tensor<float> input, int mb, int oc, int oh, int ow,
+            int poolHeight, int poolWidth,
+            int strideHeight, int strideWidth,
+            int padHeight, int padWidth,
+            int[] switches)
+        {
+            int IH = input.Dimensions[2];
+            int IW = input.Dimensions[3];
+            int KH = poolHeight;
+            int KW = poolWidth;
+            int SH = strideHeight;
+            int SW = strideWidth;
+            int padT = padHeight;
+            int padL = padWidth;
+
+            float d = float.NegativeInfinity;
+            var src = input;
+            var srcData = input.Data;
+
+            var srcOffSet = mb * src.DimensionOffSets[0] + oc * src.DimensionOffSets[1];
+
+            for (int kh = 0; kh < KH; ++kh)
+            {
+                int ih = oh * SH - padT + kh;
+                var srchOffSet = srcOffSet + src.DimensionOffSets[2] * ih;
+                for (int kw = 0; kw < KW; ++kw)
+                {
+                    int iw = ow * SW - padL + kw;
+
+                    if (ih < 0 || ih >= IH) continue;
+                    if (iw < 0 || iw >= IW) continue;
+
+                    var srcIndex = srchOffSet + iw;
+                    var s = srcData[srcIndex];
+
+                    if (s > d)
+                    {
+                        d = s;
+                        switches[srcIndex] = kh * KW + kw;
+                    }
+                }
+            }
+
+            return d;
+        }
+    }
+}
