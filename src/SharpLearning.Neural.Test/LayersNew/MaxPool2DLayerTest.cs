0b958973c1a8ce13960c570bf065321d152f991a
mdabros <mdabros@users.noreply.github.com>
2017-05-07 19:06:27 +0200

    Fixed bug in MaxPool2DLayer

--- src/SharpLearning.Neural.Test/LayersNew/MaxPool2DLayerTest.cs
+++ src/SharpLearning.Neural.Test/LayersNew/MaxPool2DLayerTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Containers.Tensors;
 using SharpLearning.Neural.LayersNew;
 
 namespace SharpLearning.Neural.Test.LayersNew
@@ -17,6 +20,176 @@ namespace SharpLearning.Neural.Test.LayersNew
             sut.Initialize(input, executor, new Random());
 
             GradientCheckTools.CheckLayer(sut, executor, input, new Random(21));
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_Forward_1()
+        {
+            var storage = new NeuralNetStorage();
+            var inputData = new double[] { 3, 0, 0, 6,
+                                           0, 2, 3, 0,
+                                           0, 8, 10, 0,
+                                           4, 6, 0, 7 };
+
+            var input = Variable.Create(1, 1, 4, 4);
+
+            storage.AssignTensor(input, inputData);
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, new Random(23));
+            sut.Forward(storage);
+
+            var expected = Tensor<double>.Build(new double[] { 3, 6, 8, 10 }, 1, 1, 2, 2);
+            var actual = storage.GetTensor(sut.Output);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_Forward_2()
+        {
+            var storage = new NeuralNetStorage();
+            var inputData = new double[] { 3, 0, 0, 6,
+                                            0, 2, 3, 0,
+                                            0, 8, 10, 0,
+                                            4, 6, 0, 7,
+                                            4, 0, 2, 0,
+                                            0, 8, 3, 5,
+                                            10, 0, 12, 0,
+                                            6, 5, 3, 2 };
+
+            var input = Variable.Create(2, 1, 4, 4);
+
+            storage.AssignTensor(input, inputData);
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, new Random(23));
+            sut.Forward(storage);
+
+            var expected = Tensor<double>.Build(new double[] { 3, 6, 8, 10,
+                                                               8, 5, 10, 12 }, 2, 1, 2, 2);
+            var actual = storage.GetTensor(sut.Output);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_Backward_1()
+        {
+            var storage = new NeuralNetStorage();
+            var inputData = new double[] { 3, 0, 0, 6,
+                                           0, 2, 3, 0,
+                                           0, 8, 10, 0,
+                                           4, 6, 0, 7 };
+
+            var input = Variable.Create(1, 1, 4, 4);
+
+            storage.AssignTensor(input, inputData);
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, new Random(23));
+            sut.Forward(storage);
+
+            storage.AssignGradient(sut.Output, () => 1.0);
+
+            sut.Backward(storage);
+
+            var actual = storage.GetGradient(sut.Input);
+            var expected = Tensor<double>.Build(new double[] { 1, 0, 0, 1,
+                                                               0, 0, 0, 0,
+                                                               0, 1, 1, 0,
+                                                               0, 0, 0, 0 }, 1, 1, 4, 4);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_Backward_2()
+        {
+            var storage = new NeuralNetStorage();
+            var inputData = new double[] { 3, 0, 0, 6,
+                                            0, 2, 3, 0,
+                                            0, 8, 10, 0,
+                                            4, 6, 0, 7,
+                                            4, 0, 2, 0,
+                                            0, 8, 3, 5,
+                                            10, 0, 12, 0,
+                                            6, 5, 3, 2 };
+
+            var input = Variable.Create(2, 1, 4, 4);
+
+            storage.AssignTensor(input, inputData);
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, new Random(23));
+            sut.Forward(storage);
+
+            storage.AssignGradient(sut.Output, () => 1.0);
+
+            sut.Backward(storage);
+
+            var actual = storage.GetGradient(sut.Input);
+            var expected = Tensor<double>.Build(new double[] { 1, 0, 0, 1,
+                                                               0, 0, 0, 0,
+                                                               0, 1, 1, 0,
+                                                               0, 0, 0, 0,
+                                                               0, 0, 0, 0,
+                                                               0, 1, 0, 1,
+                                                               1, 0, 1, 0,
+                                                               0, 0, 0, 0 }, 2, 1, 4, 4);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_MultipleForwardsPasses()
+        {
+            var random = new Random(32);
+            var storage = new NeuralNetStorage();
+            var input = Variable.Create(10, 3, 20, 20);
+
+            storage.AssignTensor(input, () => random.NextDouble());
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, new Random(23));
+
+            sut.Forward(storage);
+            var output = storage.GetTensor(sut.Output);
+            var expected = Tensor<double>.Build(output.Data.ToArray(),
+                output.Dimensions.ToArray());
+
+            for (int i = 0; i < 20; i++)
+            {
+                sut.Forward(storage);
+                var actual = storage.GetTensor(sut.Output);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void MaxPool2DLayer_MultipleBackwardsPasses()
+        {
+            var random = new Random(232);
+            var storage = new NeuralNetStorage();
+
+            var input = Variable.Create(2, 1, 4, 4);
+            storage.AssignTensor(input, () => random.Next(256));
+
+            var sut = new MaxPool2DLayer(2, 2);
+            sut.Initialize(input, storage, random);
+
+            sut.Forward(storage);
+            storage.AssignGradient(sut.Output, () => 1.0);
+
+            sut.Backward(storage);
+            var inputGradient = storage.GetGradient(sut.Input);
+
+            var expected = Tensor<double>.Build(inputGradient.Data.ToArray(),
+                inputGradient.Dimensions.ToArray());
+
+            for (int i = 0; i < 20; i++)
+            {
+                sut.Backward(storage);
+                var actual = storage.GetGradient(sut.Input);
+
+                Trace.WriteLine(string.Join(",", expected.Data));
+                Trace.WriteLine(string.Join(",", actual.Data));
+
+                Assert.AreEqual(expected, actual);
+            }
         }
     }
 }
--- src/SharpLearning.Neural/Providers/DotNetOp/MaxPool2D.cs
+++ src/SharpLearning.Neural/Providers/DotNetOp/MaxPool2D.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
-using SharpLearning.Containers.Tensors;
+using SharpLearning.Containers.Extensions;
 using SharpLearning.Neural.LayersNew;
 
 namespace SharpLearning.Neural.Providers.DotNetOp
@@ -39,6 +38,7 @@ namespace SharpLearning.Neural.Providers.DotNetOp
 
             var srcData = src.Data;
             var dstData = dst.Data;
+
 
             Parallel.For(0, MB, mb =>
             {
@@ -89,7 +89,7 @@ namespace SharpLearning.Neural.Providers.DotNetOp
                                 }
                             }
 
-                            if(training)
+                            if (training)
                             {
                                 m_switchX[mb][n] = winx;
                                 m_switchY[mb][n] = winy;
@@ -128,6 +128,7 @@ namespace SharpLearning.Neural.Providers.DotNetOp
             var OH = outputGradient.Dimensions[2];
             var OW = outputGradient.Dimensions[3];
 
+            inputGradient.Data.Clear();
             var inputGradientData = inputGradient.Data;
             var outputGradientData = outputGradient.Data;
 
@@ -165,95 +166,5 @@ namespace SharpLearning.Neural.Providers.DotNetOp
                 }
             });
         }
-
-        #region MKL IMPL
-
-        static void Forward(Tensor<float> input,
-            int poolHeight, int poolWidth,
-            int strideH, int strideW,
-            int padH, int padW,
-            int[] switches,
-            Tensor<float> output)
-        {
-            var src = input;
-            var dst = output;
-
-            int MB = src.Dimensions[0];
-            int OC = src.Dimensions[1];
-            int OH = dst.Dimensions[2];
-            int OW = dst.Dimensions[3];
-
-            var dstData = dst.Data;
-            //for (int mb = 0; mb < MB; ++mb)
-            Parallel.For(0, MB, mb =>
-            {
-                var dstBOffSet = dst.DimensionOffSets[0] * mb;
-                for (int oc = 0; oc < OC; ++oc)
-                {
-                    var dstcOffSet = dstBOffSet + dst.DimensionOffSets[1] * oc;
-                    for (int oh = 0; oh < OH; ++oh)
-                    {
-                        var dsthOffSet = dstcOffSet + dst.DimensionOffSets[2] * oh;
-                        for (int ow = 0; ow < OW; ++ow)
-                        {
-                            var dstIndex = dsthOffSet + ow;
-                            dstData[dstIndex] = MaxPoolForward(input, mb, oc, oh, ow,
-                                poolHeight, poolWidth,
-                                strideH, strideW,
-                                padH, padW, switches);
-                        }
-                    }
-                }
-            });
-        }
-
-        static float MaxPoolForward(Tensor<float> input, int mb, int oc, int oh, int ow,
-            int poolHeight, int poolWidth,
-            int strideHeight, int strideWidth,
-            int padHeight, int padWidth,
-            int[] switches)
-        {
-            int IH = input.Dimensions[2];
-            int IW = input.Dimensions[3];
-            int KH = poolHeight;
-            int KW = poolWidth;
-            int SH = strideHeight;
-            int SW = strideWidth;
-            int padT = padHeight;
-            int padL = padWidth;
-
-            float d = float.NegativeInfinity;
-            var src = input;
-            var srcData = input.Data;
-
-            var srcOffSet = mb * src.DimensionOffSets[0] + oc * src.DimensionOffSets[1];
-
-            for (int kh = 0; kh < KH; ++kh)
-            {
-                int ih = oh * SH - padT + kh;
-                var srchOffSet = srcOffSet + src.DimensionOffSets[2] * ih;
-
-                for (int kw = 0; kw < KW; ++kw)
-                {
-                    int iw = ow * SW - padL + kw;
-
-                    if (ih < 0 || ih >= IH) continue;
-                    if (iw < 0 || iw >= IW) continue;
-
-                    var srcIndex = srchOffSet + iw;
-                    var s = srcData[srcIndex];
-
-                    if (s > d)
-                    {
-                        d = s;
-                        switches[srcIndex] = kh * KW + kw;
-                    }
-                }
-            }
-
-            return d;
-        }
-
-        # endregion
     }
 }
