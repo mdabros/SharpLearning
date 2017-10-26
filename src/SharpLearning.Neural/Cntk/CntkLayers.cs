c526898620e802cb3fd989f3d56f646f45070ba3
mdabros <mdabros@users.noreply.github.com>
2017-10-26 17:49:17 +0200

    SharpLearning.Neural: Add BatchNormalizationLayer

--- src/SharpLearning.Neural/Cntk/CntkLayers.cs
+++ src/SharpLearning.Neural/Cntk/CntkLayers.cs
@@ -101,5 +101,18 @@ namespace SharpLearning.Neural.Cntk
         {
             return CNTKLib.Dropout(input, dropoutRate, seed);
         }
+
+        public static Function BatchNormalizationLayer(Variable input, bool spatial, 
+            double initialScaleValue = 1, double initialBiasValue = 0, int bnTimeConst = 5000)
+        {
+            var biasParams = new Parameter(new int[] { NDShape.InferredDimension }, (float)initialBiasValue, Device, "");
+            var scaleParams = new Parameter(new int[] { NDShape.InferredDimension }, (float)initialScaleValue, Device, "");
+            var runningMean = new Constant(new int[] { NDShape.InferredDimension }, 0.0f, Device);
+            var runningInvStd = new Constant(new int[] { NDShape.InferredDimension }, 0.0f, Device);
+            var runningCount = Constant.Scalar(0.0f, Device);
+
+            return CNTKLib.BatchNormalization(input, scaleParams, biasParams, runningMean, runningInvStd, runningCount,
+                spatial, (double)bnTimeConst, 0.0, 1e-5 /* epsilon */);
+        }
     }
 }
