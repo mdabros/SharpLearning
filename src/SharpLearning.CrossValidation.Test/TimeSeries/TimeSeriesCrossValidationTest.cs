8802eab15f30d6d17d8b038c22db762fef4c4aa8
mada@ihfood.dk <mada@ihfood.dk>
2017-11-21 21:13:10 +0100

    SharpLearning.CrossValidation: Add initial TimeSeriesCrossValidation class and simple test

---
+++ src/SharpLearning.CrossValidation.Test/TimeSeries/TimeSeriesCrossValidationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.CrossValidation.Test.Properties;
+using SharpLearning.CrossValidation.TimeSeries;
+using SharpLearning.DecisionTrees.Learners;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.Metrics.Regression;
+
+namespace SharpLearning.CrossValidation.Test.TimeSeries
+{
+    [TestClass]
+    public class TimeSeriesCrossValidationTest
+    {
+        [TestMethod]
+        public void TimeSeriesCrossValidation_Validate()
+        {
+            var targetName = "T";
+            var parser = new CsvParser(() => new StringReader(Resources.DecisionTreeData));
+            var observations = parser.EnumerateRows(v => !v.Contains(targetName)).ToF64Matrix();
+            var targets = parser.EnumerateRows(targetName).ToF64Vector();
+            var initialTrainingSize = 5;
+
+            var sut = new TimeSeriesCrossValidation<double>(initialTrainingSize);
+            var timeSeriesPredictions = sut.Validate(new RegressionDecisionTreeLearner(), observations, targets);
+            var timeSeriesTargets = targets.Skip(initialTrainingSize).ToArray();
+
+            var metric = new MeanSquaredErrorRegressionMetric();
+            var error = metric.Error(timeSeriesTargets, timeSeriesPredictions);
+
+            Assert.AreEqual(0.097833163747046217, error, 0.00001);
+        }
+    }
+}
---
+++ src/SharpLearning.CrossValidation/TimeSeries/TimeSeriesCrossValidation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpLearning.Common.Interfaces;
+using SharpLearning.Containers.Matrices;
+
+namespace SharpLearning.CrossValidation.TimeSeries
+{
+    /// <summary>
+    /// Time series cross-validation. Based on rolling validation using the original order of the data.
+    /// Using the specified initial size of the training set, a model is trained. 
+    /// The model predicts the first observation following the training data. 
+    /// Following, this data point is included in the training and a new model is trained,
+    /// which predict the next observation. This continous until all observations following the initial training size,
+    /// has been validated.
+    /// </summary>
+    /// <typeparam name="TPrediction"></typeparam>
+    public sealed class TimeSeriesCrossValidation<TPrediction>
+    {
+        readonly int m_initialTrainingSize;
+
+        /// <summary>
+        /// Time series cross-validation. Based on rolling validation.
+        /// </summary>
+        /// <param name="initialTrainingSize">The initial size of the training set.</param>
+        public TimeSeriesCrossValidation(int initialTrainingSize)
+        {
+            if (initialTrainingSize <= 0) throw new ArgumentException($"initialTrainingSize much be larger than 0, was {initialTrainingSize}");
+            m_initialTrainingSize = initialTrainingSize;
+        }
+
+        /// <summary>
+        /// Time series cross-validation. Based on rolling validation using the original order of the data.
+        /// Using the specified initial size of the training set, a model is trained. 
+        /// The model predicts the first observation following the training data. 
+        /// Following, this data point is included in the training and a new model is trained,
+        /// which predict the next observation. This continous until all observations following the initial training size,
+        /// has been validated.
+        /// </summary>
+        /// <param name="learner"></param>
+        /// <param name="observations"></param>
+        /// <param name="targets"></param>
+        /// <returns>The validated predictions, following the initial training size</returns>
+        public TPrediction[] Validate(IIndexedLearner<TPrediction> learner, F64Matrix observations, double[] targets)
+        {
+            if (observations.RowCount != targets.Length)
+            { throw new ArgumentException($"observation row count {observations.RowCount} must match target length {targets.Length}"); }
+
+            if (m_initialTrainingSize >= observations.RowCount)
+            { throw new ArgumentException($"observation row count {observations.RowCount} is smaller than initial training size {m_initialTrainingSize}"); }
+
+            var trainingIndices = Enumerable.Range(0, m_initialTrainingSize).ToArray();
+            var predictionLength = targets.Length - trainingIndices.Length;
+            var predictions = new TPrediction[predictionLength];
+
+            var observation = new double[observations.ColumnCount];
+            var currentObservationIndex = trainingIndices.Length;
+
+            for (int i = 0; i < predictions.Length; i++)
+            {
+                var model = learner.Learn(observations, targets, trainingIndices);
+                observations.Row(currentObservationIndex, observation);
+                predictions[i] = model.Predict(observation);
+
+                currentObservationIndex++;
+                trainingIndices = Enumerable.Range(0, currentObservationIndex).ToArray();
+            }
+
+            return predictions;
+        }
+    }
+}
