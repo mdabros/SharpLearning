9eda65ca2b1762322abf11a4cdd4bc78b6e48207
mada <mada@ihfood.dk>
2017-11-23 13:03:53 +0100

    SharpLearning.Neural.Test: Add cntk logistic regression example using cntk optimizer/trainer

---
+++ src/SharpLearning.Neural.Test/Cntk/CntkLogisticRegressionTest.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using CNTK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpLearning.Neural.Test.Cntk
+{
+    [TestClass]
+    public class CntkLogisticRegressionTest
+    { 
+
+        [TestMethod]
+        public void Run_Cntk_Logistic_Regression()
+        {
+            // select execution device
+            var device = DeviceDescriptor.CPUDevice;
+
+            // setup problem dimensions
+            var inputDim = 3;
+            var numOutputClasses = 2;
+
+            // setup variables for handling input to the model
+            Variable featureVariable = Variable.InputVariable(new int[] { inputDim }, DataType.Float);
+            Variable targetVariable = Variable.InputVariable(new int[] { numOutputClasses }, DataType.Float);
+            
+            // create a logistic regression model
+            Function lr = CreateLogisticRegression(featureVariable, numOutputClasses, device);
+
+            // setup loss and eval error
+            Function loss = CNTKLib.CrossEntropyWithSoftmax(lr, targetVariable);
+            Function evalError = CNTKLib.ClassificationError(lr, targetVariable);
+
+            // setup learner
+            TrainingParameterScheduleDouble learningRatePerSample = new CNTK.TrainingParameterScheduleDouble(0.02, 1);
+            var parameterLearners = new List<Learner>() { Learner.SGDLearner(lr.Parameters(), learningRatePerSample) };
+            var trainer = Trainer.CreateTrainer(lr, loss, evalError, parameterLearners);
+
+            // learning hyper-parameters
+            int minibatchSize = 64;
+            int numMinibatchesToTrain = 10000;
+            int updatePerMinibatches = 50;
+
+            // mini batch training loop
+            for (int minibatchCount = 0; minibatchCount < numMinibatchesToTrain; minibatchCount++)
+            {
+                // generate next minibatch
+                Dictionary<Variable, Value> miniBatch = GenerateBatchData(minibatchSize, inputDim, numOutputClasses,
+                    featureVariable, targetVariable, device);
+
+                // train model with mini batch
+                trainer.TrainMinibatch(miniBatch, false, device);
+
+                // print progress
+                TraceProgress(trainer, updatePerMinibatches, minibatchCount);
+            }
+        }
+
+        static Function CreateLogisticRegression(Variable input, int outputDim, DeviceDescriptor device)
+        {
+            int inputDim = input.Shape[0];
+            var weightParam = new Parameter(new int[] { outputDim, inputDim }, DataType.Float, 1, device, "w");
+            var biasParam = new Parameter(new int[] { outputDim }, DataType.Float, 0, device, "b");
+            
+            var linearModel = CNTKLib.Times(weightParam, input) + biasParam;
+
+            return CNTKLib.Sigmoid(linearModel);
+        }
+
+        static Dictionary<Variable, Value> GenerateBatchData(int sampleSize, int inputDim, int numOutputClasses,
+            Variable featureVariable, Variable targetVariable, DeviceDescriptor device)
+        {
+            float[] features;
+            float[] oneHottargets;
+            DataGenerator.GenerateRawDataSamples(sampleSize, inputDim, numOutputClasses, out features, out oneHottargets);
+
+            // these should be disposed.
+            var featureValue = Value.CreateBatch<float>(new int[] { inputDim }, features, device);
+            var targetValue = Value.CreateBatch<float>(new int[] { numOutputClasses }, oneHottargets, device);
+
+            return new Dictionary<Variable, Value>() { { featureVariable, featureValue }, { targetVariable, targetValue } };
+        }
+
+        static void TraceProgress(Trainer trainer, int updatePerMinibatches, int minibatchCount)
+        {
+            if ((minibatchCount % updatePerMinibatches) == 0 && trainer.PreviousMinibatchSampleCount() != 0)
+            {
+                float trainLossValue = (float)trainer.PreviousMinibatchLossAverage();
+                float evaluationValue = (float)trainer.PreviousMinibatchEvaluationAverage();
+
+                Trace.WriteLine($"Minibatch: {minibatchCount} CrossEntropyLoss = {trainLossValue}, EvaluationCriterion = {evaluationValue}");
+            }
+        }
+    }
+}
---
+++ src/SharpLearning.Neural.Test/Cntk/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLearning.Neural.Test.Cntk
+{
+    public static class DataGenerator
+    {
+        public static void GenerateRawDataSamples(int sampleSize, int inputDim, int numOutputClasses,
+            out float[] features, out float[] oneHotLabels)
+        {
+            Random random = new Random(0);
+
+            features = new float[sampleSize * inputDim];
+            oneHotLabels = new float[sampleSize * numOutputClasses];
+
+            for (int sample = 0; sample < sampleSize; sample++)
+            {
+                int label = random.Next(numOutputClasses);
+                for (int i = 0; i < numOutputClasses; i++)
+                {
+                    oneHotLabels[sample * numOutputClasses + i] = label == i ? 1 : 0;
+                }
+
+                for (int i = 0; i < inputDim; i++)
+                {
+                    features[sample * inputDim + i] = (float)GenerateGaussianNoise(3, 1, random) * (label + 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+        /// https://stackoverflow.com/questions/218060/random-gaussian-variables
+        /// </summary>
+        /// <returns></returns>
+        static double GenerateGaussianNoise(double mean, double stdDev, Random random)
+        {
+            double u1 = 1.0 - random.NextDouble();
+            double u2 = 1.0 - random.NextDouble();
+            double stdNormalRandomValue = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+            return mean + stdDev * stdNormalRandomValue;
+        }
+    }
+}
