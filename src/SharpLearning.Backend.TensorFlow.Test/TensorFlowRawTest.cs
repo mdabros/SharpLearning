114eae31712475c1564576e4c23c806ee62bde49
ntr <ntr@ihfood.dk>
2018-01-15 13:41:51 +0100

    raw

--- src/SharpLearning.Backend.TensorFlow.Test/TensorFlowRawTest.cs
+++ src/SharpLearning.Backend.TensorFlow.Test/TensorFlowRawTest.cs
@@ -16,17 +16,29 @@ namespace SharpLearning.Backend.TensorFlow.Test
             {
                 var one = g.Const(new TFTensor(1f), "one");
                 var two = g.Const(new TFTensor(2f), "two");
+                var p = g.Placeholder(TFDataType.Float, null, "p");
                 var add = g.Add(one, two, "add");
+                var mul = g.Mul(add, p, "mul");
+
+                
 
                 using (var s = new TFSession(g))
                 {
                     var runner = s.GetRunner();
                     var r = runner.Run(add);
 
+
                     Assert.AreEqual(TFDataType.Float, r.TensorType);
                     Assert.AreEqual(3f, r.GetValue());
 
                     Trace.WriteLine(r);
+
+                    using (var buffer = new TFBuffer())
+                    {
+                        g.ToGraphDef(buffer);
+                        var bytes = buffer.ToArray();
+                        // TODO: Save bytes, try load in tensor board
+                    }
                 }
             }
         }
