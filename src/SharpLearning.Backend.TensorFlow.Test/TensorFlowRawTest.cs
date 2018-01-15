342be2f8bfba4b17f2cef758019f9e7ef2245465
ntr <ntr@ihfood.dk>
2018-01-15 13:34:31 +0100

    Add simple raw tensorf flow test

--- src/SharpLearning.Backend.TensorFlow.Test/TensorFlowRawTest.cs
+++ src/SharpLearning.Backend.TensorFlow.Test/TensorFlowRawTest.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TensorFlow;
 
 namespace SharpLearning.Backend.TensorFlow.Test
 {
     [TestClass]
     public class TensorFlowRawTest
     {
+        [TestMethod]
+        public void TensorFlowRawTest_Simple()
+        {
+            using (var g = new TFGraph())
+            {
+                var one = g.Const(new TFTensor(1f), "one");
+                var two = g.Const(new TFTensor(2f), "two");
+                var add = g.Add(one, two, "add");
 
+                using (var s = new TFSession(g))
+                {
+                    var runner = s.GetRunner();
+                    var r = runner.Run(add);
+
+                    Assert.AreEqual(TFDataType.Float, r.TensorType);
+                    Assert.AreEqual(3f, r.GetValue());
+
+                    Trace.WriteLine(r);
+                }
+            }
+        }
     }
 }
