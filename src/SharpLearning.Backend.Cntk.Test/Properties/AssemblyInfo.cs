1ffd4b75a6fcb50fd263a1fd8992a6e813b6c5d6
mada <mada@ihfood.dk>
2018-01-17 10:08:31 +0100

    Add .net framework SharpLearning.Backend.Cntk.Test project

---
+++ src/SharpLearning.Backend.Cntk.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpLearning.Backend.Cntk.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpLearning.Backend.Cntk.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5a4c66b-ddf0-45c5-9e4a-b2b0ceb28f72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
