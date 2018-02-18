869dd6fa63584d8b19440d918516c8ab2541cc76
mada@ihfood.dk <mada@ihfood.dk>
2018-02-18 15:08:20 +0100

    Fix usings

--- src/SharpLearning.Optimization.Test/OptimizerParameters/RandomUniformTest.cs
+++ src/SharpLearning.Optimization.Test/OptimizerParameters/RandomUniformTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Optimization.OptimizerParameters;
 
 namespace SharpLearning.Optimization.Test.OptimizerParameters
 {
