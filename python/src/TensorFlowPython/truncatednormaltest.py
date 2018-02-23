80294b98f222a1682bdd2665dbe83182991f3f4c
nietras <nietras@users.noreply.github.com>
2018-02-23 13:18:30 +0100

    Started work on separate truncated_normal test.

---
+++ python/src/TensorFlowPython/truncatednormaltest.py
@@ -0,0 +1,69 @@
+# https://www.tensorflow.org/get_started/mnist/pros
+# Modified slightly to have intermediate variables
+
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# Disable linter warnings to maintain consistency with tutorial.
+# pylint: disable=invalid-name
+# pylint: disable=g-bad-import-order
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tempfile
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+
+FLAGS = None
+
+GlobalSeed = 42
+
+def weight_variable(shape):
+  """weight_variable generates a weight variable of a given shape."""
+  initial = tf.truncated_normal(shape, stddev=0.1, seed=GlobalSeed)
+  return tf.Variable(initial)
+
+
+def bias_variable(shape):
+  """bias_variable generates a bias variable of a given shape."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+def main(_):
+  w = weight_variable([2, 3])
+
+  with tf.Session() as sess:
+    initializeOutpu = sess.run(tf.global_variables_initializer())
+
+    globalVariables = tf.global_variables()
+    for g in globalVariables:
+        # .initial_value()
+        gp = tf.Print(g, [g])
+        print(gp.eval())
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data_dir', type=str,
+                      default='/tmp/tensorflow/mnist/input_data',
+                      help='Directory for storing input data')
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
---
+++ src/SharpLearning.Backend.TensorFlow.Test/TensorFlowRawTruncatedNormalTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Backend.Testing;
+using TensorFlow;
+
+namespace SharpLearning.Backend.TensorFlow.Test
+{
+    [TestClass]
+    public class TensorFlowRawTruncatedNormalTest
+    {
+        const int GlobalSeed = 42;
+
+        [TestInitialize]
+        public void Check64Bit()
+        {
+            Assert.IsTrue(Environment.Is64BitProcess, "Must execute as 64-bit");
+        }
+
+        [TestMethod]
+        public void TensorFlowRawTruncatedNormalTest_2x3()
+        {
+            using (var g = new TFGraph())
+            {
+                var variables = WeightVariable(g, new TFShape(2, 3));
+
+                TFOutput[] variablesOutputs = new TFOutput[] { variables.variable };
+                TFOperation[] variablesAssignOps = new TFOperation[] { variables.variable.Operation };
+
+                using (var session = new TFSession(g))
+                {
+                    TFOperation[] targets = null; // TODO: It is possible to create a single operation target, instead of using outputs... how?
+                    TFBuffer runMetaData = null;
+                    TFBuffer runOptions = null;
+                    TFStatus trainStatus = new TFStatus();
+
+                    // Initialize variables
+                    session.GetRunner().AddTarget(variablesAssignOps).Run();
+
+                    // Dump initial assigns to see random initializes are identical
+                    TFTensor[] variablesInitialized = session.Run(new TFOutput[] { }, new TFTensor[] { }, variablesOutputs,
+                        targets, runMetaData, runOptions, trainStatus);
+
+                    trainStatus.Raise();
+
+
+                    foreach (var v in variablesInitialized)
+                    {
+                        var vText = v.ToString();
+                        var array = (Array)v.GetValue();
+                        Trace.WriteLine(vText);
+                        Trace.WriteLine(array.ToDebugText());
+                    }
+                    // Can be used for debugging initialization of all variables
+                    //using (var w = new StreamWriter("MnistDeepVariablesInitial.txt"))
+                    //{
+                    //    foreach (var v in variablesInitialized)
+                    //    {
+                    //        var vText = v.ToString();
+                    //        var array = (Array)v.GetValue();
+                    //        w.WriteLine(vText);
+                    //        w.WriteLine(array.ToDebugText());
+                    //        Log(vText + " Initialize Logged");
+                    //    }
+                    //}
+                }
+            }
+        }
+
+        public static (TFOutput assign, TFOutput variable) WeightVariable(TFGraph g, TFShape shape)
+        {
+            const float mean = 0.0f;
+            const float stddev = 0.1f;
+            TFOutput shape_output = g.Const(shape.AsTensor());
+            TFOutput rnd = g.TruncatedNormal(shape_output, TFDataType.Float, seed: GlobalSeed);
+            TFTensor mean_tensor = new TFTensor(mean);
+            TFTensor stddev_tensor = new TFTensor(stddev);
+            TFOutput mean_output = g.Const(mean_tensor);
+            TFOutput stddev_output = g.Const(stddev_tensor);
+            TFOutput mul = g.Mul(rnd, stddev_output);
+            TFOutput value = g.Add(mul, mean_output);
+            TFOutput initial = value;
+            //TFOutput initial = g.ParameterizedTruncatedNormal(shape_output, TFDataType.Float, seed: GlobalSeed);
+            //with ops.name_scope(name, "truncated_normal", [shape, mean, stddev]) as name:
+            //shape_tensor = _ShapeTensor(shape)
+            //mean_tensor = ops.convert_to_tensor(mean, dtype = dtype, name = "mean")
+            //stddev_tensor = ops.convert_to_tensor(stddev, dtype = dtype, name = "stddev")
+            //seed1, seed2 = random_seed.get_seed(seed)
+            //rnd = gen_random_ops._truncated_normal(
+            //    shape_tensor, dtype, seed = seed1, seed2 = seed2)
+            //mul = rnd * stddev_tensor
+            //value = math_ops.add(mul, mean_tensor, name = name)
+            //return value
+
+            // What about std dev???
+            //Variable variable = g.Variable(initial);
+
+            TFOutput w = g.VariableV2(shape, TFDataType.Float, operName: "W");
+            TFOutput w_init = g.Assign(w, initial);
+
+            return (w_init, w);
+        }
+
+    }
+}
