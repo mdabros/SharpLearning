164deb2890854fee3563f9bc9e563e2751797190
mada@ihfood.dk <mada@ihfood.dk>
2018-03-11 15:06:22 +0100

    Delete unused util method

--- src/SharpLearning.Neural.Cntk/CntkUtils.cs
+++ src/SharpLearning.Neural.Cntk/CntkUtils.cs
@@ -12,22 +12,6 @@ namespace SharpLearning.Neural.Cntk
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="a1"></param>
-        /// <param name="a2"></param>
-        public static void ConvertArray(double[] a1, float[] a2)
-        {
-            if (a1.Length != a2.Length)
-            { throw new ArgumentException("a1 length: " + a1.Length + " differs from " + a2.Length); }
-
-            for (int i = 0; i < a1.Length; i++)
-            {
-                a2[i] = (float)a1[i];
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
         internal static ParameterVector AsParameterVector(IList<Parameter> input)
