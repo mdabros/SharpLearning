e08add3667f832c25b0d7cae1d0717a670fc5d67
Mads Dabros <mada@ihfood.dk>
2018-03-13 15:00:33 +0100

    Add initial initializers

--- src/SharpLearning.Neural.Cntk/Initializers.cs
+++ src/SharpLearning.Neural.Cntk/Initializers.cs
@@ -26,27 +26,92 @@ namespace SharpLearning.Neural.Cntk
     /// </summary>
     public static class Initializers
     {
-        public static CNTKDictionary Create(Initializer initializer,
-            double scale,
-            uint seed = 42)
+        public static CNTKDictionary Create(Initializer initializer)
         {
             switch (initializer)
             {
                 case Initializer.Uniform:
-                    return CNTKLib.UniformInitializer(scale, seed);
+                    return Uniform();
                 case Initializer.Normal:
+                    return Normal();
                 case Initializer.TruncatedNormal:
+                    return TruncatedNormal();
                 case Initializer.Zeros:
+                    return CNTKLib.ConstantInitializer(0);
                 case Initializer.Ones:
-                case Initializer.Constant:
+                    return CNTKLib.ConstantInitializer(1);
                 case Initializer.Xavier:
+                    return Xavier();
                 case Initializer.GlorotNormal:
+                    return GlorotNormal();
                 case Initializer.GlorotUniform:
+                    return GlorotUniform();
                 case Initializer.HeNormal:
+                    return HeNormal();
                 case Initializer.HeUniform:
+                    return HeUniform();
                 default:
                     throw new ArgumentException("Unsupported initializer: " + initializer);
             }
+        }
+
+        static CNTKDictionary Uniform(uint seed = 1)
+        {
+            return CNTKLib.UniformInitializer(CNTKLib.DefaultParamInitScale, seed);
+        }
+
+        static CNTKDictionary Normal(uint seed = 1)
+        {
+            return CNTKLib.NormalInitializer(CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
+        }
+
+        static CNTKDictionary TruncatedNormal(uint seed = 1)
+        {
+            return CNTKLib.TruncatedNormalInitializer(CNTKLib.DefaultParamInitScale,
+                seed);
+        }
+
+        static CNTKDictionary Xavier(uint seed = 1)
+        {
+            return CNTKLib.XavierInitializer(CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
+        }
+
+        static CNTKDictionary GlorotNormal(uint seed = 1)
+        {
+            return CNTKLib.GlorotNormalInitializer(CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
+        }
+
+        static CNTKDictionary GlorotUniform(uint seed = 1)
+        {
+            return CNTKLib.GlorotUniformInitializer(CNTKLib.DefaultParamInitScale, 
+                CNTKLib.SentinelValueForInferParamInitRank, 
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
+        }
+
+        static CNTKDictionary HeNormal(uint seed = 1)
+        {
+            return CNTKLib.HeNormalInitializer(CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
+        }
+
+        static CNTKDictionary HeUniform(uint seed = 1)
+        {
+            return CNTKLib.HeUniformInitializer(CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                seed);
         }
     }
 }
