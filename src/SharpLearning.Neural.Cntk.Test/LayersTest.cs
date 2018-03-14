a3b3764fdd05bd460e9c2ee44b410cd742beab84
Mads Dabros <mada@ihfood.dk>
2018-03-14 08:52:49 +0100

    Add Dense_No_Bias test

--- src/SharpLearning.Neural.Cntk.Test/LayersTest.cs
+++ src/SharpLearning.Neural.Cntk.Test/LayersTest.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using CNTK;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -17,15 +17,39 @@ namespace SharpLearning.Neural.Cntk.Test
             var inputVariable = CNTKLib.InputVariable(new int[] { 2 }, dataType);
             var data = new float[] { 1, 1 };
 
-            var sut = Layers.Dense(inputVariable, 2);
+            var sut = Layers.Dense(inputVariable, 2, weightInitializer: Initializer.Ones, biasInitializer: Initializer.Ones);
 
             var input = new Dictionary<Variable, Value> { { inputVariable, Value.CreateBatch(inputVariable.Shape, data, 0, data.Length, device)} };
             var output = new Dictionary<Variable, Value> { { sut.Output, null } };
             sut.Evaluate(input, output, true, device);
 
-            var actual = output[sut.Output].GetDenseData<float>(sut.Output).Single();
+            var actual = output[sut.Output].GetDenseData<float>(sut.Output)
+                .Single().Single();
 
-            Assert.AreEqual(1.2, actual);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void Dense_No_Bias()
+        {
+            var dataType = DataType.Float;
+            var device = DeviceDescriptor.CPUDevice;
+
+            var inputVariable = CNTKLib.InputVariable(new int[] { 2 }, dataType);
+            var data = new float[] { 1, 1 };
+
+            var sut = Layers.Dense(inputVariable, 2, 
+                weightInitializer: Initializer.Ones, 
+                bias: false);
+
+            var input = new Dictionary<Variable, Value> { { inputVariable, Value.CreateBatch(inputVariable.Shape, data, 0, data.Length, device) } };
+            var output = new Dictionary<Variable, Value> { { sut.Output, null } };
+            sut.Evaluate(input, output, true, device);
+
+            var actual = output[sut.Output].GetDenseData<float>(sut.Output)
+                .Single().Single();
+
+            Assert.AreEqual(2, actual);
         }
     }
 }
