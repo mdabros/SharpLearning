1479eaba178e287814da2e7894c0b881a9031ca5
Mads Dabros <mada@ihfood.dk>
2018-03-14 11:45:52 +0100

    Add initial LearnersTest

---
+++ src/SharpLearning.Neural.Cntk.Test/LearnersTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using CNTK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpLearning.Neural.Cntk.Test
+{
+    [TestClass]
+    public class LearnersTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SGD_Parameters_Is_Null_Throws()
+        {
+            Learners.SGD(null);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SGD_LearningRate_Below_Zero_Throws()
+        {
+            Learners.SGD(new List<Parameter>(), learningRate: -0.1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SGD_L1Regularization_Below_Zero_Throws()
+        {
+            Learners.SGD(new List<Parameter>(), l1Regularization: -0.1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SGD_L2Regularization_Below_Zero_Throws()
+        {
+            Learners.SGD(new List<Parameter>(), l2Regularization: -0.1);
+        }
+
+        [TestMethod]
+        public void SetAdditionalOptions()
+        {
+            var expectedL1Regularization = 0.1;
+            var expectedL2Regularization = 0.001;
+
+            var sut = Learners.SetAdditionalOptions(expectedL1Regularization, expectedL2Regularization);
+
+            Assert.AreEqual(expectedL1Regularization, sut.l1RegularizationWeight);
+            Assert.AreEqual(expectedL2Regularization, sut.l2RegularizationWeight);
+        }
+    }
+}
--- src/SharpLearning.Neural.Cntk/Learners.cs
+++ src/SharpLearning.Neural.Cntk/Learners.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CNTK;
 
 namespace SharpLearning.Neural.Cntk
@@ -13,19 +14,22 @@ namespace SharpLearning.Neural.Cntk
         /// </summary>
         /// <param name="parameters">Learnable parameters of the model</param>
         /// <param name="learningRate">Learning rate. (Default is 0.001)</param>
-        /// <param name="l1Reguralization">L1 reguralization term. (Default is 0, so no reguralization)</param>
-        /// <param name="l2Reguralization">L2 reguralization term. (Default is 0, so no reguralization)</param>
-        /// <param name="unitGain"></param>
+        /// <param name="l1Regularization">L1 regularization term. (Default is 0, so no regularization)</param>
+        /// <param name="l2Regularization">L2 regularization term. (Default is 0, so no regularization)</param>
         /// <returns></returns>
         public static Learner SGD(IList<Parameter> parameters,
             double learningRate = 0.01,
-            double l1Reguralization = 0.0,
-            double l2Reguralization = 0.0,
-            bool unitGain = true)
+            double l1Regularization = 0.0,
+            double l2Regularization = 0.0)
         {
+            if (parameters == null) { throw new ArgumentNullException("parameters"); }
+            if (learningRate < 0.0) { throw new ArgumentException(nameof(learningRate) + " Has to be larger or equal to 0"); }
+            if (l1Regularization < 0.0) { throw new ArgumentException(nameof(l1Regularization) + " Has to be larger or equal to 0"); }
+            if (l2Regularization < 0.0) { throw new ArgumentException(nameof(l2Regularization) + " Has to be larger or equal to 0"); }
+
             var learningRatePerSample = new TrainingParameterScheduleDouble(learningRate, 1);
 
-            var options = SetupAdditionalOptions(l1Reguralization, l2Reguralization);
+            var options = SetAdditionalOptions(l1Regularization, l2Regularization);
 
             return CNTKLib.SGDLearner(CntkUtils.AsParameterVector(parameters),
                 learningRatePerSample, options);
@@ -37,20 +41,20 @@ namespace SharpLearning.Neural.Cntk
         /// <param name="parameters">Learnable parameters of the model</param>
         /// <param name="learningRate">Learning rate. (Default is 0.001)</param>
         /// <param name="momentum">Momentum (default is 0.9).</param>
-        /// <param name="l1Reguralization">L1 reguralization term. (Default is 0, so no reguralization)</param>
-        /// <param name="l2Reguralization">L2 reguralization term. (Default is 0, so no reguralization)</param>
+        /// <param name="l1Regularization">L1 regularization term. (Default is 0, so no regularization)</param>
+        /// <param name="l2Regularization">L2 regularization term. (Default is 0, so no regularization)</param>
         /// <param name="unitGain"></param>
         /// <returns></returns>
         public static Learner MomentumSGD(IList<Parameter> parameters, 
             double learningRate = 0.01, double momentum = 0.1,
-            double l1Reguralization = 0.0,
-            double l2Reguralization = 0.0,
+            double l1Regularization = 0.0,
+            double l2Regularization = 0.0,
             bool unitGain = true)
         {
             var learningRatePerSample = new TrainingParameterScheduleDouble(learningRate, 1);
             var momentumPerSample = new TrainingParameterScheduleDouble(momentum, 1);
 
-            var options = SetupAdditionalOptions(l1Reguralization, l2Reguralization);
+            var options = SetAdditionalOptions(l1Regularization, l2Regularization);
 
             return CNTKLib.MomentumSGDLearner(CntkUtils.AsParameterVector(parameters), 
                 learningRatePerSample, momentumPerSample, unitGain,
@@ -70,22 +74,22 @@ namespace SharpLearning.Neural.Cntk
         /// Note that this is the beta1 parameter in the Adam paper.</param>
         /// <param name="varianceMomentum">variance momentum schedule. (Default is 0.999). 
         /// Note that this is the beta2 parameter in the Adam paper.</param>
-        /// <param name="l1Reguralization">L1 reguralization term. (Default is 0, so no reguralization)</param>
-        /// <param name="l2Reguralization">L2 reguralization term. (Default is 0, so no reguralization)</param>
+        /// <param name="l1Regularization">L1 regularization term. (Default is 0, so no regularization)</param>
+        /// <param name="l2Regularization">L2 regularization term. (Default is 0, so no regularization)</param>
         /// <param name="unitGain"></param>
         /// <param name="epsilon"></param>
         /// <returns></returns>
         public static Learner Adam(IList<Parameter> parameters, double learningRate = 0.001, 
             double momentum = 0.9, double varianceMomentum = 0.999,              
-            double l1Reguralization = 0.0,
-            double l2Reguralization = 0.0,
+            double l1Regularization = 0.0,
+            double l2Regularization = 0.0,
             bool unitGain = true, double epsilon = 1e-08f)
         {
             var learningRatePerSample = new TrainingParameterScheduleDouble(learningRate, 1);
             var momentumRate = new TrainingParameterScheduleDouble(momentum, 1);
             var varianceMomentumRate = new TrainingParameterScheduleDouble(varianceMomentum, 1);
 
-            var options = SetupAdditionalOptions(l1Reguralization, l2Reguralization);
+            var options = SetAdditionalOptions(l1Regularization, l2Regularization);
 
             // Consider: gradientClippingWithTruncation and gradientClippingThresholdPerSample
 
@@ -99,11 +103,11 @@ namespace SharpLearning.Neural.Cntk
                 options);
         }
 
-        private static AdditionalLearningOptions SetupAdditionalOptions(double l1Reguralization, double l2Reguralization)
+        public static AdditionalLearningOptions SetAdditionalOptions(double l1Regularization, double l2Regularization)
         {
             var options = new AdditionalLearningOptions();
-            options.l1RegularizationWeight = l1Reguralization;
-            options.l2RegularizationWeight = l2Reguralization;
+            options.l1RegularizationWeight = l1Regularization;
+            options.l2RegularizationWeight = l2Regularization;
             return options;
         }
     }
