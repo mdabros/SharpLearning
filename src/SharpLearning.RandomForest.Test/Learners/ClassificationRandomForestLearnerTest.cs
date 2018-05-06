a8d5bcf4c86c53817ee005f6b9d7714daa0ce3c3
Mads Dabros <mdabros@users.noreply.github.com>
2018-05-06 12:45:29 +0200

    Fix the non-deterministic behaviour of the random forest and extra trees learners when running multi-threaded. (#67)
    * Add non-reproducible parallel test of ClassificationRanomForsetLearner
    
    * Make ClassificationRandomForestLearner reproducible when running parallel
    
    * Update failing test
    
    * Fix formatting
    
    * Make ClassificationExtremelyRandomizedTreesLearner reproducible when running parallel
    
    * Make RegressionExtremelyRandomizedTreesLearner reproducible when running parallel
    
    * Make RegressionRandomForestLearner reproducible when running parallel
    
    * Fix BayesianOptimizer tests after changes to regression random forest
    
    * Removed unused test data
    
    * Sort using
    
    * Refactor ClassificationExtremelyRandomizedTreesLearnerTest
    
    * Refactor ClassificationRandomForestLearnerTest
    
    * Refactor RegressionExtremelyRandomizedTreesLearnerTest
    
    * Refactor RegressionRandomForestLearnerTest
    
    * Refactor ClassificationForestModelTest
    
    * Refactor RegressionForestModelTest
    
    * Increment version

--- src/SharpLearning.RandomForest.Test/Learners/ClassificationRandomForestLearnerTest.cs
+++ src/SharpLearning.RandomForest.Test/Learners/ClassificationRandomForestLearnerTest.cs
@@ -1,76 +1,75 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SharpLearning.Containers.Matrices;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpLearning.Containers.Extensions;
-using SharpLearning.Containers;
+using SharpLearning.Containers.Matrices;
 using SharpLearning.InputOutput.Csv;
 using SharpLearning.Metrics.Classification;
 using SharpLearning.RandomForest.Learners;
 using SharpLearning.RandomForest.Test.Properties;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 
 namespace SharpLearning.RandomForest.Test.Learners
 {
     [TestClass]
     public class ClassificationRandomForestLearnerTest
     {
+        readonly double m_delta = 0.0000001;
+
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Aptitude_Trees_1()
         {
             var error = ClassificationRandomLearner_Learn_Aptitude(1);
-            Assert.AreEqual(0.42307692307692307, error, 0.0000001);
+            Assert.AreEqual(0.42307692307692307, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Aptitude_Trees_5()
         {
             var error = ClassificationRandomLearner_Learn_Aptitude(5);
-            Assert.AreEqual(0.38461538461538464, error, 0.0000001);
+            Assert.AreEqual(0.38461538461538464, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Aptitude_Trees_100()
         {
             var error = ClassificationRandomLearner_Learn_Aptitude(100);
-            Assert.AreEqual(0.23076923076923078, error, 0.0000001);
+            Assert.AreEqual(0.23076923076923078, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Aptitude_Trees_100_SubSample()
         {
             var error = ClassificationRandomLearner_Learn_Aptitude(100, 0.5);
-            Assert.AreEqual(0.38461538461538464, error, 0.0000001);
+            Assert.AreEqual(0.38461538461538464, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Glass_1()
         {
             var error = ClassificationRandomForestLearner_Learn_Glass(1);
-            Assert.AreEqual(0.20093457943925233, error, 0.0000001);
+            Assert.AreEqual(0.20093457943925233, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Glass_5()
         {
             var error = ClassificationRandomForestLearner_Learn_Glass(5);
-            Assert.AreEqual(0.0794392523364486, error, 0.0000001);
+            Assert.AreEqual(0.0560747663551402, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Glass_100()
         {
             var error = ClassificationRandomForestLearner_Learn_Glass(100);
-            Assert.AreEqual(0.018691588785046728, error, 0.0000001);
+            Assert.AreEqual(0.018691588785046728, error, m_delta);
         }
 
         [TestMethod]
         public void ClassificationRandomForestLearner_Learn_Glass_100_SubSample()
         {
             var error = ClassificationRandomForestLearner_Learn_Glass(100, 0.5);
-            Assert.AreEqual(0.056074766355140186, error, 0.0000001);
+            Assert.AreEqual(0.056074766355140186, error, m_delta);
         }
 
         [TestMethod]
@@ -95,7 +94,26 @@ namespace SharpLearning.RandomForest.Test.Learners
             var evaluator = new TotalErrorClassificationMetric<double>();
             var error = evaluator.Error(targets, predictions);
 
-            Assert.AreEqual(0.098130841121495324, error, 0.0000001);
+            Assert.AreEqual(0.098130841121495324, error, m_delta);
+        }
+
+        [TestMethod]
+        public void ClassificationRandomForestLearner_Learn_Glass_100_Trees_Parallel()
+        {
+            var parser = new CsvParser(() => new StringReader(Resources.Glass));
+            var observations = parser.EnumerateRows(v => v != "Target").ToF64Matrix();
+            var targets = parser.EnumerateRows("Target").ToF64Vector();
+
+            var sut = new ClassificationRandomForestLearner(100, 1, 100, 1, 0.0001, 1.0, 42, true);
+
+            var model = sut.Learn(observations, targets);
+
+            var predictions = model.Predict(observations);
+
+            var evaluator = new TotalErrorClassificationMetric<double>();
+            var error = evaluator.Error(targets, predictions);
+
+            Assert.AreEqual(0.018691588785046728, error, m_delta);
         }
 
         double ClassificationRandomForestLearner_Learn_Glass(int trees, double subSampleRatio = 1.0)
