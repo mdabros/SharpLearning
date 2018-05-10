bda06a19f227faf78422e46fd88dfe4775fdcdbc
mdabros <mdabros@users.noreply.github.com>
2018-05-10 18:24:21 +0200

    Add save/load method to ClassificationXGBoostModel. This sadly needs to use the filesystem directly because of how the XGBoost models are saved. Also, the target name to target index mapping needs to be saved separately. XGBoost expects the mapping to be sequential from [0;numberOfClasses].

---
+++ src/SharpLearning.XGBoost.Test/Models/ClassificationXGBoostModelTest.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.Metrics.Classification;
+using SharpLearning.XGBoost.Learners;
+using SharpLearning.XGBoost.Models;
+using SharpLearning.XGBoost.Test.Properties;
+
+namespace SharpLearning.XGBoost.Test.Learners
+{
+    [TestClass]
+    public class ClassificationXGBoostModelTest
+    {
+        readonly double m_delta = 0.0000001;
+
+        [TestMethod]
+        public void ClassificationXGBoostModel_Save_Load()
+        {
+            var parser = new CsvParser(() => new StringReader(Resources.Glass));
+            var observations = parser.EnumerateRows(v => v != "Target").ToF64Matrix();
+            var targets = parser.EnumerateRows("Target").ToF64Vector();
+
+            var leaner = new ClassificationXGBoostLearner(estimaters: 2);
+            var modelFilePath = "model.xgb";
+
+            using (var sutPreSave = leaner.Learn(observations, targets))
+            {
+                AssertModel(observations, targets, sutPreSave);
+                sutPreSave.Save(modelFilePath);
+            }
+
+            using (var sutAfterSave = ClassificationXGBoostModel.Load(modelFilePath))
+            {
+                AssertModel(observations, targets, sutAfterSave);
+            }
+        }
+
+        void AssertModel(Containers.Matrices.F64Matrix observations, double[] targets, ClassificationXGBoostModel model)
+        {
+            var predictions = model.Predict(observations);
+            var evaluator = new TotalErrorClassificationMetric<double>();
+            var actual = evaluator.Error(targets, predictions);
+
+            Assert.AreEqual(0.17757009345794392, actual, m_delta);
+        }
+    }
+}
--- src/SharpLearning.XGBoost/Models/ClassificationXGBoostModel.cs
+++ src/SharpLearning.XGBoost/Models/ClassificationXGBoostModel.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using SharpLearning.Common.Interfaces;
 using SharpLearning.Containers.Matrices;
+using SharpLearning.InputOutput.Serialization;
 using XGBoost.lib;
 
 namespace SharpLearning.XGBoost.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public sealed class ClassificationXGBoostModel : IDisposable, IPredictorModel<double>
     {
         readonly Booster m_model;
         readonly Dictionary<float, double> m_targetIndexToTargetName;
 
+        const string m_targetIndexToTargetNameFileName = "targetIndexToTargetName.xml";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="targetIndexToTargetName"></param>
         public ClassificationXGBoostModel(Booster model, Dictionary<float, double> targetIndexToTargetName)
         {
             if (model == null) throw new ArgumentNullException(nameof(model));
@@ -20,6 +32,11 @@ namespace SharpLearning.XGBoost.Models
             m_targetIndexToTargetName = targetIndexToTargetName;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="observation"></param>
+        /// <returns></returns>
         public double Predict(double[] observation)
         {
             var floatObservation = new float[][]
@@ -33,6 +50,11 @@ namespace SharpLearning.XGBoost.Models
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="observations"></param>
+        /// <returns></returns>
         public double[] Predict(F64Matrix observations)
         {
             var floatObservations = observations.ToFloatJaggedArray();
@@ -44,16 +66,67 @@ namespace SharpLearning.XGBoost.Models
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public double[] GetRawVariableImportance()
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="featureNameToIndex"></param>
+        /// <returns></returns>
         public Dictionary<string, double> GetVariableImportance(Dictionary<string, int> featureNameToIndex)
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Loads a ClassificationXGBoostModel.
+        /// </summary>
+        /// <param name="modelFilePath"></param>
+        /// <returns></returns>
+        public static ClassificationXGBoostModel Load(string modelFilePath)
+        {
+            // Load target mapping.
+            var targetIndexToTargetNameFilePath = GetTargetIndexToTargetNameFilePath(modelFilePath);
+            var targetIndexToTargetName = new GenericXmlDataContractSerializer()
+                .Deserialize<Dictionary<float, double>>(() => new StreamReader(targetIndexToTargetNameFilePath));
+
+            // load XGBoost model.
+            return new ClassificationXGBoostModel(new Booster(modelFilePath), targetIndexToTargetName);
+        }
+
+        /// <summary>
+        /// Saves the ClassificationXGBoostModel.
+        /// </summary>
+        /// <param name="modelFilePath"></param>
+        public void Save(string modelFilePath)
+        {
+            // Save XGBoost model.
+            m_model.Save(modelFilePath);
+
+            // Save target mapping.
+            var targetIndexToTargetNameFilePath = GetTargetIndexToTargetNameFilePath(modelFilePath);
+
+            new GenericXmlDataContractSerializer()
+                .Serialize(m_targetIndexToTargetName, 
+                    () => new StreamWriter(targetIndexToTargetNameFilePath));
+        }
+
+        static string GetTargetIndexToTargetNameFilePath(string modelFilePath)
+        {
+            return Path.Combine(Path.GetDirectoryName(modelFilePath),
+                m_targetIndexToTargetNameFileName);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
             if (m_model != null)
