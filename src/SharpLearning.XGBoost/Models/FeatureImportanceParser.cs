80b142ea26fb8fc020ad1777315bc2db06daeff0
mdabros <mdabros@users.noreply.github.com>
2018-05-13 09:56:48 +0200

    Add FeatureImportanceParser

---
+++ src/SharpLearning.XGBoost/Models/FeatureImportanceParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SharpLearning.XGBoost.Models
+{
+    public static class FeatureImportanceParser
+    {
+        public static double[] ParseFromTreeDump(string[] textTrees, int numberOfFeatures)
+        {
+            var importanceType = new string[] { "gain=" };
+            var newLine = new string[] { Environment.NewLine };
+
+            var rawFeatureImportance = new double[numberOfFeatures];
+            foreach (var tree in textTrees)
+            {
+                var lines = tree.Split(newLine, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in lines)
+                {
+                    if(!line.Contains("[") || line.Contains("booster"))
+                    {
+                        // Continue if line does not contain a split.
+                        continue;
+                    }
+
+                    // Extract feature name from string between []
+                    var name = line.Split('[')[1].Split(']')[0].Split('<')[0];
+
+                    // Extract featureIndex
+                    var featureIndex = int.Parse(name.Split('f')[1]);
+
+                    // extract gain or cover
+                    var gain = double.Parse(line.Split(importanceType, 
+                        StringSplitOptions.RemoveEmptyEntries)[1].Split(',')[0]);
+
+                    // add to featureImportance
+                    rawFeatureImportance[featureIndex] += gain;
+                }
+            }
+
+            return rawFeatureImportance;
+        }               
+    }
+}
