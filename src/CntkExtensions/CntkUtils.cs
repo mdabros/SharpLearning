5fada5a496cac2d46584337b4bdd18313bd33cee
mdabros <mdabros@users.noreply.github.com>
2018-08-11 14:33:20 +0200

    Delete unused methods

--- src/CntkExtensions/CntkUtils.cs
+++ src/CntkExtensions/CntkUtils.cs
@@ -23,25 +23,5 @@ namespace CntkExtensions
             }
             return inputVector;
         }
-
-        public static CNTK.Value GetTensors(CNTK.NDShape shape, float[] src, int[] indices, int indices_begin, int indices_end, CNTK.DeviceDescriptor device)
-        {
-            var cpu_tensors = Util.get_minibatch_data_CPU(shape, src, indices, indices_begin, indices_end);
-            var result = CNTK.Value.CreateBatch(shape, cpu_tensors, device, true);
-            return result;
-        }
-
-        static float[] get_minibatch_data_CPU(CNTK.NDShape shape, float[] src, int[] indices, int indices_begin, int indices_end)
-        {
-            var num_indices = indices_end - indices_begin;
-            var row_length = shape.TotalSize;
-            var result = new float[num_indices];
-            var row_index = 0;
-            for (var index = indices_begin; index != indices_end; index++)
-            {
-                result[row_index++] = src[indices[index]];
-            }
-            return result;
-        }
     }
 }
