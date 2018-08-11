6737bec034b87ae8708af6f413d2968838b8f02e
mdabros <mdabros@users.noreply.github.com>
2018-08-11 15:48:55 +0200

    Refactor artificial data method

--- src/CntkExtensions.Test/Models/SequentialTest.cs
+++ src/CntkExtensions.Test/Models/SequentialTest.cs
@@ -11,20 +11,10 @@ namespace CntkExtensions.Test.Models
         [TestMethod]
         public void Sequential_Use_Case()
         {
-            var observationCount = 10000;
             var inputShape = new int[] { 28, 28, 1 };
-            var inputSize = inputShape.Aggregate((d1, d2) => d1 * d2);
             var outputShape = 10;
 
-            var random = new Random(32);
-            var observationsData = new float[observationCount * inputSize];
-            observationsData = observationsData.Select(v => (float)random.NextDouble()).ToArray();
-            var observations = new Tensor(observationsData, 28, 28, 1, observationCount);
-            
-            var targetsData = new float[observationCount];
-            targetsData = targetsData.Select(d => (float)random.Next(10)).ToArray();
-            var oneHotTargetsData = targetsData.EncodeOneHot();
-            var targets = new Tensor(oneHotTargetsData, outputShape, observationCount);
+            (var observations, var targets) = ArtificialData(inputShape, outputShape);
 
             var network = new Sequential(Layers.Input(inputShape));
 
@@ -32,13 +22,30 @@ namespace CntkExtensions.Test.Models
             network.Add(x => Layers.ReLU(x));
             network.Add(x => Layers.Dense(x, units: outputShape));
 
-            network.Compile(p => Learners.MomentumSGD(p), 
-               (t, p) => Losses.CategoricalCrossEntropy(t, p), 
+            network.Compile(p => Learners.MomentumSGD(p),
+               (t, p) => Losses.CategoricalCrossEntropy(t, p),
                (t, p) => Metrics.Accuracy(t, p));
 
             network.Fit(observations, targets, batchSize: 32, epochs: 5);
+        }
 
+        static (Tensor observations, Tensor targets) ArtificialData(int[] inputShape, int outputShape)
+        {
+            var observationCount = 10000;
+            var inputSize = inputShape.Aggregate((d1, d2) => d1 * d2);
+            outputShape = 10;
+            var random = new Random(32);
 
+            var observationsData = new float[observationCount * inputSize];
+            observationsData = observationsData.Select(v => (float)random.NextDouble()).ToArray();
+            var observations = new Tensor(observationsData, 28, 28, 1, observationCount);
+
+            var targetsData = new float[observationCount];
+            targetsData = targetsData.Select(d => (float)random.Next(10)).ToArray();
+            var oneHotTargetsData = targetsData.EncodeOneHot();
+            var targets = new Tensor(oneHotTargetsData, outputShape, observationCount);
+
+            return (observations, targets);
         }
     }
 }
