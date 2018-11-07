99fc24e180b4aac4e09e13ec05cc75764a5bf931
mdabros <mdabros@users.noreply.github.com>
2018-11-07 22:00:53 +0100

    Remove unnecessary members

--- src/CntkCatalyst/CntkMinibatchSource.cs
+++ src/CntkCatalyst/CntkMinibatchSource.cs
@@ -6,18 +6,16 @@ namespace CntkCatalyst
     public class CntkMinibatchSource : IMinibatchSource
     {
         readonly MinibatchSource m_minibatchSource;
-        readonly string m_featuresName;
-        readonly string m_targetsName;
 
         public CntkMinibatchSource(MinibatchSource minibatchSource, string featuresName, string labelsName)
         {
             m_minibatchSource = minibatchSource ?? throw new ArgumentNullException(nameof(minibatchSource));
-            m_featuresName = featuresName;
-            m_targetsName = labelsName;
+            FeaturesName = featuresName;
+            TargetsName = labelsName;
         }
 
-        public string FeaturesName => m_featuresName;
-        public string TargetsName => m_targetsName;
+        public string FeaturesName { get; }
+        public string TargetsName { get; }
 
         public UnorderedMapStreamInformationMinibatchData GetNextMinibatch(uint minibatchSizeInSamples, 
             DeviceDescriptor device)
