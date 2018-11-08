f869ab9e058748f2ad40f310c342c3ada51eaa19
mdabros <mdabros@users.noreply.github.com>
2018-11-08 19:05:58 +0100

    Add Embedding layer

---
+++ src/CntkCatalyst/LayerFunctions/Layers.Embedding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNTK;
+
+namespace CntkCatalyst.LayerFunctions
+{
+    /// <summary>
+    /// Layer operations for CNTK
+    /// </summary>
+    public static partial class Layers
+    {
+        /// <summary>
+        /// based on the Embedding from: https://github.com/Microsoft/CNTK/blob/master/bindings/python/cntk/layers/layers.py      
+        /// </summary>
+        public static Function Embedding(this Function input, int shape, CNTKDictionary initializer,
+            DataType dataType, DeviceDescriptor device)
+        {
+            var weightsShape = new int[] { shape, CNTK.NDShape.InferredDimension };
+            var weights = new Parameter( weightsShape, dataType, initializer, device);
+            var result = CNTKLib.Times(weights, input);
+            
+            return result;
+        }
+    }
+}
