958468155fc3d9fd50e63a0e55f91d8d75bcd6b2
mdabros <mdabros@users.noreply.github.com>
2018-11-08 18:35:44 +0100

    Add dropout

---
+++ src/CntkCatalyst/LayerFunctions/Layers.Dropout.cs
@@ -0,0 +1,15 @@
+﻿using CNTK;
+
+namespace CntkCatalyst.LayerFunctions
+{
+    /// <summary>
+    /// Layer operations for CNTK
+    /// </summary>
+    public static partial class Layers
+    {
+        public static Function Dropout(this Function input, double dropoutRate, uint seed)
+        {
+            return CNTKLib.Dropout(input, dropoutRate, seed);
+        }
+    }
+}
