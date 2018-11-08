804f5b4fa4aa3ef467d6157ac9e585985f657534
mdabros <mdabros@users.noreply.github.com>
2018-11-08 21:04:34 +0100

    Add flatten

---
+++ src/CntkCatalyst/LayerFunctions/Layers.Flatten.cs
@@ -0,0 +1,15 @@
+﻿using CNTK;
+
+namespace CntkCatalyst.LayerFunctions
+{
+    /// <summary>
+    /// Layer operations for CNTK
+    /// </summary>
+    public static partial class Layers
+    {
+        public static Function Flatten(this Function input)
+        {
+            return CNTKLib.Flatten(input);
+        }
+    }
+}
