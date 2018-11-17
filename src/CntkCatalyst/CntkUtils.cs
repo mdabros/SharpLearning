c14a61e02e119e8fbd3b002f84917ebb933ec8c6
mdabros <mdabros@users.noreply.github.com>
2018-11-17 14:55:05 +0100

    Remove onehot

--- src/CntkCatalyst/CntkUtils.cs
+++ src/CntkCatalyst/CntkUtils.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using CNTK;
 
 namespace CntkCatalyst
@@ -27,34 +26,6 @@ namespace CntkCatalyst
                 boolVector.Add(fill);
             }
             return boolVector;
-        }
-
-        /// <summary>
-        /// Encodes targets in a one-of-n structure. Target vector of with two classes [0, 1, 1, 0] becomes a matrix:
-        /// 1 0
-        /// 0 1
-        /// 0 1
-        /// 1 0
-        /// Primary use is for classification
-        /// </summary>
-        public static float[] EncodeOneHot(this float[] targets)
-        {
-            var index = 0;
-            var targetNameToTargetIndex = targets.Distinct().OrderBy(v => v)
-                .ToDictionary(v => v, v => index++);
-
-            var distinctTargets = targetNameToTargetIndex.Count;
-            var oneHot = new float[targets.Length * distinctTargets];
-
-            for (int i = 0; i < targets.Length; i++)
-            {
-                var target = targets[i];
-                var targetIndex = targetNameToTargetIndex[target];
-                var oneHotIndex = i * distinctTargets + targetIndex;
-                oneHot[oneHotIndex] = 1.0f;
-            }
-
-            return oneHot;
         }
     }
 }
