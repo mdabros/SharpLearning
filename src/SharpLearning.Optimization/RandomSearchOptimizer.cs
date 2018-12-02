5b8c684a88d6761b232dc34825b5d7c39f957268
mdabros <mdabros@users.noreply.github.com>
2018-12-02 15:24:33 +0100

    Refactor RandomSearchOptimizer to use IParameter

--- src/SharpLearning.Optimization/RandomSearchOptimizer.cs
+++ src/SharpLearning.Optimization/RandomSearchOptimizer.cs
@@ -13,7 +13,7 @@ namespace SharpLearning.Optimization
     public sealed class RandomSearchOptimizer : IOptimizer
     {
         readonly bool m_runParallel;
-        readonly MinMaxParameter[] m_parameters;
+        readonly IParameter[] m_parameters;
         readonly int m_iterations;
         readonly IParameterSampler m_sampler;
 
@@ -25,7 +25,7 @@ namespace SharpLearning.Optimization
         /// <param name="iterations">The number of iterations to perform</param>
         /// <param name="seed"></param>
         /// <param name="runParallel">Use multi threading to speed up execution (default is true)</param>
-        public RandomSearchOptimizer(MinMaxParameter[] parameterRanges, int iterations, int seed=42, bool runParallel = true)
+        public RandomSearchOptimizer(IParameter[] parameterRanges, int iterations, int seed=42, bool runParallel = true)
         {
             if (parameterRanges == null) { throw new ArgumentNullException("parameterRanges"); }
             m_parameters = parameterRanges;
@@ -36,15 +36,13 @@ namespace SharpLearning.Optimization
 
         /// <summary>
         /// Random search optimizer initializes random parameters between min and max of the provided.
-        /// Returns the result which best minimises the provided function.
+        /// Returns the result which best minimizes the provided function.
         /// </summary>
         /// <param name="functionToMinimize"></param>
         /// <returns></returns>
-        public OptimizerResult OptimizeBest(Func<double[], OptimizerResult> functionToMinimize)
-        {
+        public OptimizerResult OptimizeBest(Func<double[], OptimizerResult> functionToMinimize) =>
             // Return the best model found.
-            return Optimize(functionToMinimize).First();
-        }
+            Optimize(functionToMinimize).First();
 
         /// <summary>
         /// Random search optimizer initializes random parameters between min and max of the provided
@@ -55,23 +53,23 @@ namespace SharpLearning.Optimization
         public OptimizerResult[] Optimize(Func<double[], OptimizerResult> functionToMinimize)
         {
             // Generate the cartesian product between all parameters
-            var grid = CreateNewSearchSpace(m_parameters);
+            var parameterSets = CreateParameterSets(m_parameters);
 
             // Initialize the search
             var results = new ConcurrentBag<OptimizerResult>();
 
             if(!m_runParallel)
             {
-                foreach (var param in grid)
+                foreach (var parameterSet in parameterSets)
                 {
                     // Get the current parameters for the current point
-                    var result = functionToMinimize(param);
+                    var result = functionToMinimize(parameterSet);
                     results.Add(result);
                 }
             }
             else
             {
-                var rangePartitioner = Partitioner.Create(grid, true);
+                var rangePartitioner = Partitioner.Create(parameterSets, true);
                 Parallel.ForEach(rangePartitioner, (param, loopState) =>
                 {
                     // Get the current parameters for the current point
@@ -86,7 +84,7 @@ namespace SharpLearning.Optimization
         }
 
 
-        double[][] CreateNewSearchSpace(MinMaxParameter[] parameters)
+        double[][] CreateParameterSets(IParameter[] parameters)
         {
             var newSearchSpace = new double[m_iterations][];
             for (int i = 0; i < newSearchSpace.Length; i++)
