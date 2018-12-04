ccd41de4959bc3795456df5af6495500dc8d52d1
mdabros <mdabros@users.noreply.github.com>
2018-12-04 08:49:23 +0100

    Add initial RefactorBrainStormTest

---
+++ src/SharpLearning.Neural.Test/RefactorBrainStorm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics.Tensors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpLearning.Neural.Test
+{
+    [TestClass]
+    public class RefactorBrainStormTest
+    {
+        interface ILayer
+        {
+            string Name { get; }
+            void Forward();
+            void Backward();
+        }
+
+        class DenseLayer : ILayer
+        {
+            public string Name { get; }
+
+            public void Forward()
+            {
+            }
+
+            public void Backward()
+            {
+            }
+        }
+
+        interface ILoss
+        {
+            DenseTensor<double> Losses(DenseTensor<double> targets, DenseTensor<double> predictions);
+            double AccumulateLoss(DenseTensor<double> targets, DenseTensor<double> predictions);
+        }
+
+        class MeanSquareLoss : ILoss
+        {
+            public DenseTensor<double> Losses(DenseTensor<double> targets, DenseTensor<double> predictions)
+            {
+                CheckDimensions(targets, predictions);
+
+                var losses = new DenseTensor<double>(targets.Dimensions);
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    losses[i] = predictions[i] - targets[i];
+                }
+
+                return losses;
+            }
+
+            public double AccumulateLoss(DenseTensor<double> targets, DenseTensor<double> predictions)
+            {
+                CheckDimensions(targets, predictions);
+
+                var accumulatedLoss = 0.0;
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    var error = predictions[i] - targets[i];
+                    accumulatedLoss += error * error;
+                }
+
+                accumulatedLoss = accumulatedLoss / targets.Length;
+
+                return accumulatedLoss;
+            }
+
+            static void CheckDimensions(Tensor<double> targets, Tensor<double> predictions)
+            {
+                if (targets.Dimensions != predictions.Dimensions)
+                {
+                    throw new ArgumentException($"Target dimensions: {targets.Dimensions.ToArray()} differs from "
+                        + $" Prediction dimensions: {predictions.Dimensions.ToArray()}");
+                }
+            }
+        }
+
+        class NeuralNet
+        {
+            public NeuralNet(List<ILayer> layers)
+            {
+                Layers = layers ?? throw new ArgumentNullException(nameof(layers));
+            }
+
+            public List<ILayer> Layers { get; }
+
+            public void Forward()
+            {
+                Layers.ForEach(l => l.Forward());
+            }
+
+            public void Backward()
+            {
+                Layers.ForEach(l => l.Backward());
+            }
+        }
+
+        class Optimizer
+        {
+
+        }
+
+        [TestMethod]
+        public void RefactorBrainStorm()
+        {
+
+        }
+    }
+}
