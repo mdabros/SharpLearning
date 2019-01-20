cf9e69fab15696977a8865715f7438ca70d9903b
mdabros <mdabros@users.noreply.github.com>
2019-01-20 12:26:27 +0100

    Fix usings

--- src/SharpLearning.Optimization.Test/GridSearchOptimizationTest.cs
+++ src/SharpLearning.Optimization.Test/GridSearchOptimizationTest.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SharpLearning.Optimization.Test
 {
--- src/SharpLearning.Optimization.Test/Transforms/ExponentialAverageTransformTest.cs
+++ src/SharpLearning.Optimization.Test/Transforms/ExponentialAverageTransformTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpLearning.Optimization.ParameterSamplers;
 using SharpLearning.Optimization.Transforms;
