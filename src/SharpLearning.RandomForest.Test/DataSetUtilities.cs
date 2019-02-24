12a5545b790cb04430571625cf0812ab80ee3ec2
mdabros <mdabros@users.noreply.github.com>
2019-02-24 12:33:24 +0100

    Use DataSetUtilities for loading test data sets

--- src/SharpLearning.RandomForest.Test/DataSetUtilities.cs
+++ src/SharpLearning.RandomForest.Test/DataSetUtilities.cs
@@ -14,5 +14,13 @@ namespace SharpLearning.RandomForest.Test
             var targets = parser.EnumerateRows("Pass").ToF64Vector();
             return (observations, targets);
         }
+
+        public static (F64Matrix observations, double[] targets) LoadGlassDataSet()
+        {
+            var parser = new CsvParser(() => new StringReader(Resources.Glass));
+            var observations = parser.EnumerateRows(v => v != "Target").ToF64Matrix();
+            var targets = parser.EnumerateRows("Target").ToF64Vector();
+            return (observations, targets);
+        }
     }
 }
