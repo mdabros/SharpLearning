4780910347ad78bf36e376048dbc50f8403a1d70
mdabros <mdabros@users.noreply.github.com>
2019-06-25 22:02:21 +0200

    Remove interval support in impurity metrics. These were developed for decision tree learning originally, but was replaced by alternative implementation. Also remove InCustomDictionary, which was introduced for performance reasons. This is no longer a priority since the Impurity metrics are no longer used in the learning algorithms

--- src/SharpLearning.Metrics.Test/Impurity/VarianceEntropyMetricTest.cs
+++ src/SharpLearning.Metrics.Test/Impurity/VarianceEntropyMetricTest.cs
@@ -1,24 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Containers.Views;
 using SharpLearning.Metrics.Impurity;
 
 namespace SharpLearning.Metrics.Test.Impurity
 {
     [TestClass]
-    public class GiniImpurityMetricTest
+    public class VarianceImpurityMetricTest
     {
         [TestMethod]
-        public void GiniImpurityMetric_Impurity()
+        public void VarianceEntropyMetric_Impurity()
         {
             var set1 = new double[] { 0, 1, 2, 3, 4, 3, 2, 1, 0 };
             var set2 = new double[] { 1, 1, 1, 1, 2, 2, 2, 2 };
-            var set3 = new double[] { 1, 1, 1, 1, 1, 1, 1, 1 };
+            var set3 = new double[] { 1 };
 
-            var sut = new GiniImpurityMetric();
+            var sut = new VarianceImpurityMetric();
 
             var val1 = sut.Impurity(set1);
-            Assert.AreEqual(0.79012345679012341, val1);
+            Assert.AreEqual(1.9444444444444446, val1);
             var val2 = sut.Impurity(set2);
-            Assert.AreEqual(0.5, val2);
+            Assert.AreEqual(0.2857142857142857, val2);
             var val3 = sut.Impurity(set3);
             Assert.AreEqual(0.0, val3);
         }
