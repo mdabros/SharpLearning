188987b3563490e98443fa74be8cc436f9dd1498
mdabros <mdabros@users.noreply.github.com>
2019-07-28 20:33:07 +0200

    Add CrossValidationUtilities/Test

---
+++ src/SharpLearning.CrossValidation.Test/CrossValidationUtilitiesTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.CrossValidation.Samplers;
+
+namespace SharpLearning.CrossValidation.Test
+{
+    [TestClass]
+    public class CrossValidationUtilitiesTest
+    {
+        [TestMethod]
+        public void CrossValidationUtilities_GetCrossValidationIndexSets()
+        {
+            var targets = new double[] { 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3 };
+            var sampler = new StratifiedIndexSampler<double>(seed: 242);
+            var actuals = CrossValidationUtilities.GetCrossValidationIndexSets(sampler, 
+                foldCount: 4, targets: targets);
+
+            var expecteds = new List<(int[] trainingIndices, int[] validationIndices)>
+            {
+                (new int[] { 0, 1, 2, 3, 5, 6, 7, 9, 11, 12, 13, 14 }, new int[] { 10, 4, 8 }),
+                (new int[] { 0, 1, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13 }, new int[] { 2, 7, 14 }),
+                (new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 14 }, new int[] { 5, 13, 0 }),
+                (new int[] { 0, 1, 2, 4, 5, 7, 8, 9, 10, 12, 13, 14 }, new int[] { 6, 11, 3 }),
+            };
+
+            Assert.AreEqual(expecteds.Count, actuals.Count);
+            for (int i = 0; i < expecteds.Count; i++)
+            {
+                var expected = expecteds[i];
+                var actual = actuals[i];
+                CollectionAssert.AreEqual(expected.trainingIndices, actual.trainingIndices);
+                CollectionAssert.AreEqual(expected.validationIndices, actual.validationIndices);
+            }
+        }
+
+        void TraceIndexSets(List<(int[] trainingIndices, int[] validationIndices)> indexSets)
+        {
+            const string Separator = ", ";
+            foreach (var set in indexSets)
+            {
+                Trace.WriteLine("(new int[] { " + string.Join(Separator, set.trainingIndices) + " }, " +
+                    "new int[] { " + string.Join(Separator, set.validationIndices) + " }),");
+            }
+        }
+    }
+}
---
+++ src/SharpLearning.CrossValidation/CrossValidationUtilities.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharpLearning.CrossValidation.Samplers;
+
+namespace SharpLearning.CrossValidation
+{
+    /// <summary>
+    /// Utilities for CrossValidation.
+    /// </summary>
+    public static class CrossValidationUtilities
+    {
+        /// <summary>
+        /// Returns a list of (trainingIndices, validationIndices) 
+        /// for use with k-fold cross-validation.
+        /// </summary>
+        /// <param name="sampler"></param>
+        /// <param name="foldCount"></param>
+        /// <param name="targets"></param>
+        /// <returns></returns>
+        public static List<(int[] trainingIndices, int[] validationIndices)> GetCrossValidationIndexSets(
+            IIndexSampler<double> sampler, int foldCount, double[] targets)
+        {
+            var allIndices = Enumerable.Range(0, targets.Length).ToArray();
+            return GetCrossValidationIndexSets(sampler, foldCount, targets, allIndices);
+        }
+
+        /// <summary>
+        /// Returns a list of (trainingIndices, validationIndices) 
+        /// for use with k-fold cross-validation.
+        /// </summary>
+        /// <param name="sampler"></param>
+        /// <param name="foldCount"></param>
+        /// <param name="targets"></param>
+        /// <param name="indices">indices to use for the cross validation sets</param>
+        /// <returns></returns>
+        public static List<(int[] trainingIndices, int[] validationIndices)> GetCrossValidationIndexSets(
+            IIndexSampler<double> sampler, int foldCount, double[] targets, int[] indices)
+        {
+            var samplesPerFold = targets.Length / foldCount;
+            var currentIndices = indices.ToArray();
+
+            var crossValidationIndexSets = new List<(int[] training, int[] validation)>();
+
+            for (int i = 0; i < foldCount; i++)
+            {
+                var holdoutSample = sampler.Sample(targets, samplesPerFold, currentIndices);
+                // Sample only from remaining indices.
+                currentIndices = currentIndices.Except(holdoutSample).ToArray();
+                // Training sample is all indices except the current hold out sample.
+                var trainingSample = indices.Except(holdoutSample).ToArray();
+                crossValidationIndexSets.Add((trainingSample, holdoutSample));
+            }
+
+            return crossValidationIndexSets;
+        }
+    }
+}
