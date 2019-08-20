57a247a03d278fd45a0593fdea7436e05e681d4c
mdabros <mdabros@users.noreply.github.com>
2019-08-20 20:46:47 +0200

    Add DataSourceTest serving as an initial integration test using csv loaders

---
+++ src/SharpLearning.InputOutput.Test/AssertUtilities.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.InputOutput.DataSources;
+
+namespace SharpLearning.InputOutput.Test
+{
+    public static class AssertUtilities
+    {
+        const double m_defaultDelta = 0.000001;
+
+        public static void AssertAreEqual(
+            IReadOnlyDictionary<string, DataBatch<double>> expected,
+            IReadOnlyDictionary<string, DataBatch<double>> actual,
+            double delta = m_defaultDelta)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+            foreach (var key in expected.Keys)
+            {
+                Assert.IsTrue(actual.ContainsKey(key));
+                AssertAreEqual(expected[key], actual[key]);
+            }
+        }
+
+        public static void AssertAreEqual(DataBatch<double> expected, DataBatch<double> actual, 
+            double delta = m_defaultDelta)
+        {
+            Assert.AreEqual(expected.SampleCount, actual.SampleCount);
+            CollectionAssert.AreEqual(expected.SampleShape, actual.SampleShape);
+
+            Assert.AreEqual(expected.Data.Length, actual.Data.Length);
+            for (int i = 0; i < expected.Data.Length; i++)
+            {
+                Assert.AreEqual(expected.Data[i], actual.Data[i], delta);
+            }
+        }
+    }
+}
---
+++ src/SharpLearning.InputOutput.Test/DataSources/DataSourceTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.InputOutput.DataSources;
+
+namespace SharpLearning.InputOutput.Test.DataSources
+{
+    [TestClass]
+    public class DataSourceTest
+    {
+        [TestMethod]
+        public void DataSource_Csv_GetNextBatch()
+        {
+            var idToLoaders = CreateDataLoaders();
+
+            var sut = new DataSource<double>(idToLoaders, batchSize: 3, 
+                shuffle: false, seed: 32);
+
+            var (actual, isSweepEnd) = sut.GetNextBatch();
+            var expected = new Dictionary<string, DataBatch<double>>()
+            {
+                { "observations", new DataBatch<double>(new double[] { 5, 6, 1, 15, 1, 12 }, new int[] { 2 }, 3) },
+                { "targets", new DataBatch<double>(new double[] { 0, 0, 0 }, new int[] { 1 }, 3) },
+            };
+
+            AssertUtilities.AssertAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DataSource_Csv_GetNextBatch_Shuffle()
+        {
+            var idToLoaders = CreateDataLoaders();
+
+            var sut = new DataSource<double>(idToLoaders, batchSize: 3,
+                shuffle: true, seed: 32);
+
+            var (actual, isSweepEnd) = sut.GetNextBatch();
+
+            var expected = new Dictionary<string, DataBatch<double>>()
+            {
+                { "observations", new DataBatch<double>(new double[] { 1, 12, 4, 24, 1, 8 }, new int[] { 2 }, 3) },
+                { "targets", new DataBatch<double>(new double[] { 0, 1, 0 }, new int[] { 1 }, 3) },
+            };
+
+            AssertUtilities.AssertAreEqual(expected, actual);
+        }
+
+        static Dictionary<string, DataLoader<double>> CreateDataLoaders()
+        {
+            // observations loader.
+            var observationsLoader = DataLoaders.CsvDataLoader(
+                CsvParser.FromString(DataSetUtilities.AptitudeData),
+                s => CsvRowExtensions.DefaultF64Converter(s),
+                columnNames: new[] { "AptitudeTestScore", "PreviousExperience_month" },
+                sampleShape: new[] { 2 });
+
+            // targets loader.
+            var targetsLoader = DataLoaders.CsvDataLoader(
+                CsvParser.FromString(DataSetUtilities.AptitudeData),
+                s => CsvRowExtensions.DefaultF64Converter(s),
+                columnNames: new[] { "Pass" },
+                sampleShape: new[] { 1 });
+
+            var idToLoaders = new Dictionary<string, DataLoader<double>>
+            {
+                { "observations", observationsLoader },
+                { "targets", targetsLoader },
+            };
+
+            return idToLoaders;
+        }
+    }
+}
