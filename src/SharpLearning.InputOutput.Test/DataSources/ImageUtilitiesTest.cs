8eb9aa35b66d88d079907d9f24098957f33aa135
mdabros <mdabros@users.noreply.github.com>
2019-08-26 21:43:33 +0200

    Expand file system test

--- src/SharpLearning.InputOutput.Test/DataSources/ImageUtilitiesTest.cs
+++ src/SharpLearning.InputOutput.Test/DataSources/ImageUtilitiesTest.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpLearning.InputOutput.DataSources;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace SharpLearning.InputOutput.Test.DataSources
 {
@@ -11,22 +15,29 @@ namespace SharpLearning.InputOutput.Test.DataSources
         [TestMethod]
         public void Integration_Test()
         {
-            // TODO: Add in memory tests.
-            var imageGetter = ImageSharpUtilities.GetImageLoader<Rgb24>(@"E:\mada\Dropbox\Images\image1.JPG");
-            var transforms = new ImageTransformer<Rgb24>[] 
+            // TODO: Add augmentation tests
+            var imageDirectory = @"E:\mada\Dropbox\Images\køkken spas";
+
+            var imageFilePaths = Directory.EnumerateFiles(imageDirectory);
+
+            // images data loader.
+            var random = new Random(Seed: 232);
+            var imagesGetters = DataLoaders.EnumerateImages<Rgba32>(imageFilePaths)
+                // Add augmentations
+                .Select(i => i.Resize(200, 200)
+                    .Brightness(1.0f, 3.0f, random)
+                    .Flip(FlipMode.Horizontal, random)
+                    .Skew(10, 10, random)
+                    .Rotate(10, random)
+                    .Zoom(1.1f, random));
+
+            var index = 0;
+            foreach (var imageGetter in imagesGetters)
             {
-                getter => ImageSharpUtilities.Resize(getter, 200, 200),
-                getter => ImageSharpUtilities.Rotate(getter, 90)
-            };
-
-            var imageTransformer = ImageSharpUtilities.GetImageTransformer(imageGetter, transforms);
-
-            using (var image = imageTransformer())
-            {
-                image.Save(@"E:\mada\Dropbox\Images\image1_test.JPG");
-
-                var bytes = ImageSharpUtilities.ConvertImageToBytes(imageTransformer);
-                var floats = ImageSharpUtilities.ConvertBytesToFloat(bytes);
+                using (var image = imageGetter())
+                {
+                    image.Save(Path.Combine(@"E:\test", index++.ToString() + ".JPG"));
+                }
             }
         }
     }
