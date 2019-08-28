5e121f6e0d86df53d266f21c5e65bf6af708d542
mdabros <mdabros@users.noreply.github.com>
2019-08-28 17:25:32 +0200

    Add OneHot Converter

--- src/SharpLearning.DataSource/Converters.cs
+++ src/SharpLearning.DataSource/Converters.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SharpLearning.DataSource
 {
@@ -23,6 +24,20 @@ namespace SharpLearning.DataSource
         public static double[] ToDouble(byte[] bytes)
         {
             return bytes.Select(v => (double)v).ToArray();
+        }
+
+        /// <summary>
+        /// Transform value to onehot vector.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="valueToOneHotIndex"></param>
+        /// <returns></returns>
+        public static float[] ToOneHot(float value, IReadOnlyDictionary<float, int> valueToOneHotIndex)
+        {
+            var oneHotVector = new float[valueToOneHotIndex.Count];
+            var oneHotIndex = valueToOneHotIndex[value];
+            oneHotVector[oneHotIndex] = 1;
+            return oneHotVector;
         }
     }
 }
