01725cd8c726443d2ebf4a9851ba8ac503bcd5ba
mdabros <mdabros@users.noreply.github.com>
2019-08-28 09:22:57 +0200

    Add Pad transform

--- src/SharpLearning.InputOutput/DataSources/ImageSharpUtilities.Transforms.cs
+++ src/SharpLearning.InputOutput/DataSources/ImageSharpUtilities.Transforms.cs
@@ -28,5 +28,25 @@ namespace SharpLearning.InputOutput.DataSources
             }
             return () => Transform();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TPixel"></typeparam>
+        /// <param name="imageGetter"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static ImageGetter<TPixel> Pad<TPixel>(this ImageGetter<TPixel> imageGetter, int width, int height)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            Image<TPixel> Transform()
+            {
+                var image = imageGetter();
+                image.Mutate(x => x.Pad(width, height));
+                return image;
+            }
+            return () => Transform();
+        }
     }
 }
