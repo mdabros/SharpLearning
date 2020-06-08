5e7c63f6299f855d0177e9927c6f20306c1053cd
mdabros <mdabros@users.noreply.github.com>
2020-06-08 15:55:49 +0200

    Add links to schemas and examples

--- src/SharpLearning.Onnx.Test/OnnxConversionTest.cs
+++ src/SharpLearning.Onnx.Test/OnnxConversionTest.cs
@@ -3,6 +3,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SharpLearning.Onnx
 {
+    /// <summary>
+    /// Resources:
+    /// ONNX Schema: https://github.com/onnx/onnx/blob/da13be2d8494b99807bd1f071b40c40cc6b6d1cd/onnx/defs/traditionalml/defs.cc#L872
+    /// ONNX Conversion ML.Net: https://github.com/dotnet/machinelearning/blob/master/src/Microsoft.ML.FastTree/FastTree.cs#L3030
+    /// ONNX Conversion SKLearn: https://github.com/onnx/sklearn-onnx/blob/master/skl2onnx/common/tree_ensemble.py
+    /// ONNX Conversion SKLearn: https://github.com/onnx/sklearn-onnx/blob/master/skl2onnx/operator_converters/decision_tree.py 
+    /// ONNX Conversion SKLearn: https://github.com/onnx/sklearn-onnx/blob/master/skl2onnx/operator_converters/random_forest.py
+    /// </summary>
     [TestClass]
     public class OnnxConversionTest
     {
