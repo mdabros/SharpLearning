84aebce1e1083fd50b8843427ef6cc3a78dc1779
mdabros <mdabros@users.noreply.github.com>
2020-06-08 15:39:21 +0200

    Add SharpLearning.Onnx.Test Project

---
+++ src/SharpLearning.Onnx.Test/OnnxConversionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpLearning.Onnx
+{
+    [TestClass]
+    public class OnnxConversionTest
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
