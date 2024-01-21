434aa9016795661ed61293e2dc3acfd44fb457f3
Mads Dabros <mada@ihfood.dk>
2024-01-21 18:29:18 +0100

    delete old scripts

--- all.ps1
+++
@@ -1,4 +0,0 @@
-.\restore.ps1
-.\build.cmd
-.\test.ps1
-Write-Host "Check output for errors, since scripts do not stop if errors occur." -foregroundcolor "yellow"
--- build.cmd
+++
@@ -1,2 +0,0 @@
-dotnet build /p:Configuration=Debug /m /verbosity:minimal /nologo
-dotnet build /p:Configuration=Release /m /verbosity:minimal /nologo
