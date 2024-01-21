658e05f590dd9d7468ba163f1987e0f5406f936b
Mads Dabros <mdabros@users.noreply.github.com>
2024-01-21 18:43:34 +0100

    Add .props and .targets to reduce duplication in project files  (#149)

--- clean.ps1
+++
@@ -1,9 +0,0 @@
-$buildPath = "./Build"
-$buildTestPath = "./BuildTest"
-$testResultsPath = "./TestResults"
-If (Test-Path $buildPath)
-{ Remove-Item -Confirm -Recurse -Path $buildPath }
-If (Test-Path $buildTestPath)
-{ Remove-Item -Confirm -Recurse -Path $buildTestPath }
-If (Test-Path $testResultsPath)
-{ Remove-Item -Confirm -Recurse -Path  $testResultsPath }
