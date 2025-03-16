e56bc0e5d9c999a998c484c4765dfecd0a1d8205
Mads Dabros <mada@ihfood.dk>
2025-03-16 20:06:51 +0100

    Use single row for prediction benchmark

--- src/SharpLearning.Benchmarks/Benchmarks.ClassificationModels.cs
+++ src/SharpLearning.Benchmarks/Benchmarks.ClassificationModels.cs
@@ -15,13 +15,17 @@ public static partial class Benchmarks
             DefaultLearners.LearnerNameToLearnerClassification;
         readonly Dictionary<string, IPredictorModel<double>> m_models = new();
 
+        // For creating models.
         F64Matrix m_features;
         double[] m_targets;
+        // For prediction.
+        double[] m_featureRow;
 
         [GlobalSetup]
         public void GlobalSetup()
         {
             (m_features, m_targets) = DataGenerator.GenerateClassificationData();
+            m_featureRow = m_features.Row(0);
             foreach (var (learnerName, learner) in m_learners)
             {
                 var model = learner.Learn(m_features, m_targets);
@@ -35,7 +39,7 @@ public static partial class Benchmarks
         public void Predict(string modelName)
         {
             var model = m_models[modelName];
-            model.Predict(m_features);
+            model.Predict(m_featureRow);
         }
 
         public IReadOnlyList<string> GetModels()
