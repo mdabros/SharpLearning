797707e80d17aac3e49bd9ac5be306de4154c59b
Mads Dabros <mada@ihfood.dk>
2025-03-16 19:04:04 +0100

    Refactor

--- src/SharpLearning.Benchmarks/DataGenerator.cs
+++ src/SharpLearning.Benchmarks/DataGenerator.cs
@@ -6,29 +6,35 @@ namespace SharpLearning.Benchmarks;
 public static class DataGenerator
 {
     // Default data size for benchmarks.
-    const int Rows = 1000;
-    const int Cols = 10;
-    const int MinTargetValue = 0;
-    const int MaxTargetValue = 10;
-    const int Seed = 42;
+    public const int DefaultRows = 1000;
+    public const int DefaultCols = 10;
+    public const int DefaultMinTargetValue = 0;
+    public const int DefaultMaxTargetValue = 10;
+    public const int DefaultSeed = 42;
 
-    public static (F64Matrix Features, double[] Targets) GenerateRegressionData()
+    public static (F64Matrix Features, double[] Targets) GenerateRegressionData(
+        int rows = DefaultRows, int cols = DefaultCols,
+        int seed = DefaultSeed)
     {
-        var random = new Random(Seed);
-        var targets = GenerateDoubles(Rows, 1, random);
-        var features = GenerateDoubles(Rows, Cols, random);
-        return (new F64Matrix(features, Rows, Cols), targets);
+        var random = new Random(seed);
+        var features = GenerateRandomDoubles(rows, cols, random);
+        var targets = GenerateRandomDoubles(rows, 1, random);
+        return (new F64Matrix(features, rows, cols), targets);
     }
 
-    public static (F64Matrix Features, double[] Targets) GenerateClassificationData()
+    public static (F64Matrix Features, double[] Targets) GenerateClassificationData(
+        int rows = DefaultRows, int cols = DefaultCols,
+        int minTargetValue = DefaultMinTargetValue,
+        int maxTargetValue = DefaultMaxTargetValue,
+        int seed = DefaultSeed)
     {
-        var random = new Random(Seed);
-        var targets = GenerateIntegers(Rows, 1, MinTargetValue, MaxTargetValue, random);
-        var features = GenerateDoubles(Rows, Cols, random);
-        return (new F64Matrix(features, Rows, Cols), targets);
+        var random = new Random(seed);
+        var features = GenerateRandomDoubles(rows, cols, random);
+        var targets = GenerateRandomIntegers(rows, 1, minTargetValue, maxTargetValue, random);
+        return (new F64Matrix(features, rows, cols), targets);
     }
 
-    static double[] GenerateDoubles(int rows, int cols, Random random)
+    static double[] GenerateRandomDoubles(int rows, int cols, Random random)
     {
         var data = new double[rows * cols];
         for (var i = 0; i < data.Length; i++)
@@ -38,7 +44,7 @@ public static class DataGenerator
         return data;
     }
 
-    static double[] GenerateIntegers(int rows, int cols, int min, int max, Random random)
+    static double[] GenerateRandomIntegers(int rows, int cols, int min, int max, Random random)
     {
         var data = new double[rows * cols];
         for (var i = 0; i < data.Length; i++)
